81dd6d3224c31d5c2e2be53e25936b99ae8fc11c
Sarah Wessel <swessel@broadinstitute.org>
2020-05-08 13:23:03 -0400

    Add obfuscated transformation test inputs

--- hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineBuilder.scala
+++ hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineBuilder.scala
@@ -1,8 +1,24 @@
-package org.broadinstitute.monster.dap
+package org.broadinstitute.monster.dap.transformation
 
 import com.spotify.scio.ScioContext
-import org.broadinstitute.monster.common.PipelineBuilder
+import org.broadinstitute.monster.common.{PipelineBuilder, StorageIO}
+import org.broadinstitute.monster.common.msg._
 
 object HLESurveyTransformationPipelineBuilder extends PipelineBuilder[Args] {
-  override def buildPipeline(ctx: ScioContext, args: Args): Unit = {}
+  override def buildPipeline(ctx: ScioContext, args: Args): Unit = {
+
+    // Read records in
+    val recordsDirectoryName = "records"
+    val rawValueRecords = StorageIO
+      .readJsonLists(ctx, recordsDirectoryName, s"${args.inputPrefix}/$recordsDirectoryName/*.json")
+
+    // Group by study ID (record number)
+    val valuesByStudyId = rawValueRecords
+      .groupBy(_.read[String]("record"))
+
+    // Group by field name
+    // Transform the double-grouped collections of EAV records into upack Msgs with:
+      // A key per field-name group, and
+      // The raw value(s) of the field-name-key as the object value
+  }
 }
