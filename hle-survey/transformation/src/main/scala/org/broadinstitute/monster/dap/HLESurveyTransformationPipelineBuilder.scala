d94f505b0b8e31ecd53d183408cffa799c782a2f
Dan Moran <danmoran@broadinstitute.org>
2020-05-11 14:27:21 -0400

    DSPDC-667 Finish skeleton of transformation pipeline.

--- hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineBuilder.scala
+++ hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineBuilder.scala
@@ -1,11 +1,18 @@
 package org.broadinstitute.monster.dap
 
 import com.spotify.scio.ScioContext
+import com.spotify.scio.coders.Coder
 import com.spotify.scio.values.SCollection
 import org.broadinstitute.monster.common.{PipelineBuilder, StorageIO}
 import org.broadinstitute.monster.common.msg._
+import org.broadinstitute.monster.dogaging.jadeschema.table.{HlesDog, HlesOwner}
+import upack.Msg
 
 object HLESurveyTransformationPipelineBuilder extends PipelineBuilder[Args] {
+
+  implicit val msgCoder: Coder[Msg] = Coder.beam(new UpackMsgCoder)
+
+  type RawRecord = Map[String, Array[String]]
 
   /**
     * Schedule all the steps for the Dog Aging transformation in the given pipeline context.
@@ -14,34 +21,42 @@ object HLESurveyTransformationPipelineBuilder extends PipelineBuilder[Args] {
     * is called on it.
     */
   override def buildPipeline(ctx: ScioContext, args: Args): Unit = {
+    val rawRecords = readRecords(ctx, args)
+    val dogs = rawRecords.transform("Map Dogs")(_.map(mapDog))
+    val owners = rawRecords.transform("Map Owners")(_.map(mapOwner))
 
-    /**
-      * Read in records and group by study Id, with field name subgroups.
-      * Output the format: (studyId, Iterable[(fieldName, Iterable[value])])
-      */
-    def readRecords(): SCollection[(String, Iterable[(String, Iterable[String])])] = {
-      // Read records in
-      val recordsDirectoryName = "records"
-      val rawRecords = StorageIO
-        .readJsonLists(
-          ctx,
-          recordsDirectoryName,
-          s"${args.inputPrefix}/$recordsDirectoryName/*.json"
-        )
-
-      // Group by study ID (record number) and field name
-      // to get the format: (studyId, Iterable((fieldName, Iterable(value))))
-      rawRecords
-        .groupBy(_.read[String]("record"))
-        .map {
-          case (studyId, rawRecordValues) =>
-            val rawRecordsByFieldName =
-              rawRecordValues.groupBy(_.read[String]("field_name")).map {
-                case (fieldName, rawValues) =>
-                  (fieldName, rawValues.map(_.read[String]("value")))
-              }
-            (studyId, rawRecordsByFieldName)
-        }
-    }
+    StorageIO.writeJsonLists(dogs, "Dogs", s"${args.outputPrefix}/hles_dog")
+    StorageIO.writeJsonLists(owners, "Owners", s"${args.outputPrefix}/hles_owner")
   }
+
+  /**
+    * Read in records and group by study Id, with field name subgroups.
+    * Output the format: (studyId, Iterable[(fieldName, Iterable[value])])
+    */
+  def readRecords(ctx: ScioContext, args: Args): SCollection[RawRecord] = {
+    val rawRecords = StorageIO
+      .readJsonLists(
+        ctx,
+        "Raw Records",
+        s"${args.inputPrefix}/records/*.json"
+      )
+
+    // Group by study ID (record number) and field name
+    // to get the format: (studyId, Iterable((fieldName, Iterable(value))))
+    rawRecords
+      .groupBy(_.read[String]("record"))
+      .values
+      .map { rawRecordValues =>
+        rawRecordValues
+          .groupBy(_.read[String]("field_name"))
+          .map {
+            case (fieldName, rawValues) =>
+              (fieldName, rawValues.map(_.read[String]("value")).toArray)
+          }
+      }
+  }
+
+  def mapOwner(rawRecord: RawRecord): HlesOwner = ???
+
+  def mapDog(rawRecord: RawRecord): HlesDog = ???
 }
