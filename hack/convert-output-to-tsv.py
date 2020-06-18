541ecda02416bf634e3562efbebcbb194ab9f0d2
Sarah Wessel <swessel@broadinstitute.org>
2020-06-18 17:19:57 -0400

    Have input & output directories passed in as args

--- hack/convert-output-to-tsv.py
+++ hack/convert-output-to-tsv.py
@@ -1,13 +1,17 @@
 import json
 import csv
 from os import listdir
+import sys
 
-table_names = ["hles_dog", "hles_owner"]
-schema_dir = '../repos/dog-aging-ingest/schema/src/main/jade-tables/'
-input_dir = 'dog_aging_full_transformation/'
-output_dir = 'dog_aging_tsvs/'
+if (len(sys.argv) < 3):
+    print "Please provide the input directory and output directory as arguments!"
 
-# convert each json file to tsv
+#table_names = ["hles_dog", "hles_owner"]
+table_names = ["hles_owner"]
+schema_dir = '../schema/src/main/jade-tables/'
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
+
 for table_name in table_names:
     # get set of all keys/column names from the schema file
     column_list = []
@@ -28,12 +32,11 @@ for table_name in table_names:
                 obj = json.loads(jsonObj)
                 obj_list.append(obj)
 
-    # convert to tsv and output
-    output_location = output_dir + table_name + '/' + table_name + '.tsv'
-    #output_name = table_name[4:]
+    # output to tsv
+    output_location = output_dir + '/' + table_name[5:] + '.tsv'
     with open(output_location, 'w') as output_file:
         dw = csv.DictWriter(output_file, column_list, delimiter='\t')
         dw.writeheader()
         dw.writerows(obj_list)
 
-# note: After running this script, I manually renamed the files & added "entity:" to the names of the PK columns
+# TODO: update PK name to include "entity:" in front
