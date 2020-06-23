d75347beb9eefb411a7a92ad72d395d1ad235cd6
Raaid Arshad <rarshad@broadinstitute.org>
2020-06-23 10:05:34 -0400

    move around testing inputs

--- hle-survey/transformation/src/test/scala/org/broadinstitute/monster/dap/HealthTransformationsSpec.scala
+++ hle-survey/transformation/src/test/scala/org/broadinstitute/monster/dap/HealthTransformationsSpec.scala
@@ -9,136 +9,16 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   behavior of "HealthTransformations"
   import HealthTransformations.{conditionTypes, conditions}
 
-  private val singleInfectiousDisease = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_infectious_yn" -> Array("1"),
-    "hs_dx_anaplasmosis" -> Array("1"),
-    "hs_dx_anaplasmosis_month" -> Array("2"),
-    "hs_dx_anaplasmosis_year" -> Array("2020"),
-    "hs_dx_anaplasmosis_surg" -> Array("3"),
-    "hs_dx_anaplasmosis_fu" -> Array("1")
-  )
-
-  private val multipleInfectiousDisease = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_infectious_yn" -> Array("1"),
-    "hs_dx_anaplasmosis" -> Array("1"),
-    "hs_dx_anaplasmosis_month" -> Array("2"),
-    "hs_dx_anaplasmosis_year" -> Array("2020"),
-    "hs_dx_anaplasmosis_surg" -> Array("3"),
-    "hs_dx_anaplasmosis_fu" -> Array("1"),
-    "hs_dx_plague" -> Array("1"),
-    "hs_dx_plague_month" -> Array("5"),
-    "hs_dx_plague_year" -> Array("2020"),
-    "hs_dx_plague_surg" -> Array("4"),
-    "hs_dx_plague_fu" -> Array("0"),
-    "hs_dx_infect_other" -> Array("1"),
-    "hs_dx_infect_other_spec" -> Array("falafel"),
-    "hs_dx_infect_other_month" -> Array("10"),
-    "hs_dx_infect_other_year" -> Array("2020"),
-    "hs_dx_infect_other_surg" -> Array("1"),
-    "hs_dx_infect_other_fu" -> Array("1")
-  )
-
-  private val singleEyeDisease = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_eye_yn" -> Array("1"),
-    "hs_dx_cat" -> Array("1"),
-    "hs_dx_cat_month" -> Array("2"),
-    "hs_dx_cat_year" -> Array("2020"),
-    "hs_dx_cat_surg" -> Array("3"),
-    "hs_dx_cat_fu" -> Array("1")
-  )
-
-  private val singleEyeDiseaseBlindCase1 = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_eye_yn" -> Array("1"),
-    "hs_dx_blind" -> Array("1"),
-    "hs_dx_blind_month" -> Array("5"),
-    "hs_dx_blind_year" -> Array("2020"),
-    "hs_dx_blind_surg" -> Array("4"),
-    "hs_dx_blind_fu" -> Array("0"),
-    "hs_dx_eye_cause_yn" -> Array("0")
-  )
-
-  private val singleEyeDiseaseBlindCase2 = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_eye_yn" -> Array("1"),
-    "hs_dx_blind" -> Array("1"),
-    "hs_dx_blind_month" -> Array("5"),
-    "hs_dx_blind_year" -> Array("2020"),
-    "hs_dx_blind_surg" -> Array("4"),
-    "hs_dx_blind_fu" -> Array("0"),
-    "hs_dx_eye_cause_yn" -> Array("1"),
-    "hs_dx_eye_cause" -> Array("5")
-  )
-
-  private val singleEyeDiseaseBlindCase3 = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_eye_yn" -> Array("1"),
-    "hs_dx_blind" -> Array("1"),
-    "hs_dx_blind_month" -> Array("5"),
-    "hs_dx_blind_year" -> Array("2020"),
-    "hs_dx_blind_surg" -> Array("4"),
-    "hs_dx_blind_fu" -> Array("0"),
-    "hs_dx_eye_cause_yn" -> Array("1"),
-    "hs_dx_eye_cause" -> Array("98"),
-    "hs_dx_eye_cause_other" -> Array("hummus")
-  )
-
-  private val multipleEyeDisease = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_dx_eye_yn" -> Array("1"),
-    "hs_dx_cat" -> Array("1"),
-    "hs_dx_cat_month" -> Array("2"),
-    "hs_dx_cat_year" -> Array("2020"),
-    "hs_dx_cat_surg" -> Array("3"),
-    "hs_dx_cat_fu" -> Array("1"),
-    "hs_dx_blind" -> Array("1"),
-    "hs_dx_blind_month" -> Array("5"),
-    "hs_dx_blind_year" -> Array("2020"),
-    "hs_dx_blind_surg" -> Array("4"),
-    "hs_dx_blind_fu" -> Array("0"),
-    "hs_dx_eye_cause_yn" -> Array("1"),
-    "hs_dx_eye_cause" -> Array("98"),
-    "hs_dx_eye_cause_other" -> Array("hummus"),
-    "hs_dx_eye_other" -> Array("1"),
-    "hs_dx_eye_other_spec" -> Array("falafel"),
-    "hs_dx_eye_other_month" -> Array("10"),
-    "hs_dx_eye_other_year" -> Array("2020"),
-    "hs_dx_eye_other_surg" -> Array("1"),
-    "hs_dx_eye_other_fu" -> Array("1")
-  )
-
-  private val singleCongenitalEyeDisorder = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_congenital_yn" -> Array("1"),
-    "hs_cg_disorders_yn" -> Array("1"),
-    "hs_cg_eye_cat" -> Array("1"),
-    "hs_cg_eye_cat_month" -> Array("2"),
-    "hs_cg_eye_cat_year" -> Array("2020"),
-    "hs_cg_eye_cat_surg" -> Array("3"),
-    "hs_cg_eye_cat_fu" -> Array("1")
-  )
-
-  private val multipleCongenitalEyeDisorder = Map[String, Array[String]](
-    "study_id" -> Array("10"),
-    "hs_congenital_yn" -> Array("1"),
-    "hs_cg_eye_disorders_yn" -> Array("1"),
-    "hs_cg_eye_cat" -> Array("1"),
-    "hs_cg_eye_cat_month" -> Array("2"),
-    "hs_cg_eye_cat_year" -> Array("2020"),
-    "hs_cg_eye_cat_surg" -> Array("3"),
-    "hs_cg_eye_cat_fu" -> Array("1"),
-    "hs_cg_eye_other" -> Array("1"),
-    "hs_cg_eye_other_spec" -> Array("olives"),
-    "hs_cg_eye_other_month" -> Array("2"),
-    "hs_cg_eye_other_year" -> Array("2020"),
-    "hs_cg_eye_other_surg" -> Array("3"),
-    "hs_cg_eye_other_fu" -> Array("0")
-  )
-
   it should "correctly map infectious disease values when values are defined for a single infectious disease" in {
+    val singleInfectiousDisease = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_infectious_yn" -> Array("1"),
+      "hs_dx_anaplasmosis" -> Array("1"),
+      "hs_dx_anaplasmosis_month" -> Array("2"),
+      "hs_dx_anaplasmosis_year" -> Array("2020"),
+      "hs_dx_anaplasmosis_surg" -> Array("3"),
+      "hs_dx_anaplasmosis_fu" -> Array("1")
+    )
     val exampleInfectiousDiseaseRecord = RawRecord(id = 1, singleInfectiousDisease)
     val output = HealthTransformations.mapHealthConditions(exampleInfectiousDiseaseRecord)
 
@@ -158,6 +38,26 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map infectious disease values when values are defined for multiple infectious diseases" in {
+    val multipleInfectiousDisease = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_infectious_yn" -> Array("1"),
+      "hs_dx_anaplasmosis" -> Array("1"),
+      "hs_dx_anaplasmosis_month" -> Array("2"),
+      "hs_dx_anaplasmosis_year" -> Array("2020"),
+      "hs_dx_anaplasmosis_surg" -> Array("3"),
+      "hs_dx_anaplasmosis_fu" -> Array("1"),
+      "hs_dx_plague" -> Array("1"),
+      "hs_dx_plague_month" -> Array("5"),
+      "hs_dx_plague_year" -> Array("2020"),
+      "hs_dx_plague_surg" -> Array("4"),
+      "hs_dx_plague_fu" -> Array("0"),
+      "hs_dx_infect_other" -> Array("1"),
+      "hs_dx_infect_other_spec" -> Array("falafel"),
+      "hs_dx_infect_other_month" -> Array("10"),
+      "hs_dx_infect_other_year" -> Array("2020"),
+      "hs_dx_infect_other_surg" -> Array("1"),
+      "hs_dx_infect_other_fu" -> Array("1")
+    )
     val exampleInfectiousDiseaseRecord = RawRecord(id = 1, multipleInfectiousDisease)
     val output = HealthTransformations.mapHealthConditions(exampleInfectiousDiseaseRecord)
 
@@ -207,6 +107,15 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map eye disease values when values are defined for a single eye disease" in {
+    val singleEyeDisease = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_eye_yn" -> Array("1"),
+      "hs_dx_cat" -> Array("1"),
+      "hs_dx_cat_month" -> Array("2"),
+      "hs_dx_cat_year" -> Array("2020"),
+      "hs_dx_cat_surg" -> Array("3"),
+      "hs_dx_cat_fu" -> Array("1")
+    )
     val exampleEyeDiseaseRecord = RawRecord(id = 1, singleEyeDisease)
     val output = HealthTransformations.mapHealthConditions(exampleEyeDiseaseRecord)
 
@@ -226,6 +135,16 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map eye disease values when values are defined for different cases of blindness" in {
+    val singleEyeDiseaseBlindCase1 = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_eye_yn" -> Array("1"),
+      "hs_dx_blind" -> Array("1"),
+      "hs_dx_blind_month" -> Array("5"),
+      "hs_dx_blind_year" -> Array("2020"),
+      "hs_dx_blind_surg" -> Array("4"),
+      "hs_dx_blind_fu" -> Array("0"),
+      "hs_dx_eye_cause_yn" -> Array("0")
+    )
     val exampleEyeDiseaseRecord1 = RawRecord(id = 1, singleEyeDiseaseBlindCase1)
     val output1 = HealthTransformations.mapHealthConditions(exampleEyeDiseaseRecord1)
 
@@ -243,6 +162,17 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
       row.hsFollowUpOngoing shouldBe Some(false)
     }
 
+    val singleEyeDiseaseBlindCase2 = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_eye_yn" -> Array("1"),
+      "hs_dx_blind" -> Array("1"),
+      "hs_dx_blind_month" -> Array("5"),
+      "hs_dx_blind_year" -> Array("2020"),
+      "hs_dx_blind_surg" -> Array("4"),
+      "hs_dx_blind_fu" -> Array("0"),
+      "hs_dx_eye_cause_yn" -> Array("1"),
+      "hs_dx_eye_cause" -> Array("5")
+    )
     val exampleEyeDiseaseRecord2 = RawRecord(id = 2, singleEyeDiseaseBlindCase2)
     val output2 = HealthTransformations.mapHealthConditions(exampleEyeDiseaseRecord2)
 
@@ -260,6 +190,18 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
       row.hsFollowUpOngoing shouldBe Some(false)
     }
 
+    val singleEyeDiseaseBlindCase3 = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_eye_yn" -> Array("1"),
+      "hs_dx_blind" -> Array("1"),
+      "hs_dx_blind_month" -> Array("5"),
+      "hs_dx_blind_year" -> Array("2020"),
+      "hs_dx_blind_surg" -> Array("4"),
+      "hs_dx_blind_fu" -> Array("0"),
+      "hs_dx_eye_cause_yn" -> Array("1"),
+      "hs_dx_eye_cause" -> Array("98"),
+      "hs_dx_eye_cause_other" -> Array("hummus")
+    )
     val exampleEyeDiseaseRecord3 = RawRecord(id = 2, singleEyeDiseaseBlindCase3)
     val output3 = HealthTransformations.mapHealthConditions(exampleEyeDiseaseRecord3)
 
@@ -279,6 +221,29 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map eye disease values when values are defined for multiple eye diseases" in {
+    val multipleEyeDisease = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_dx_eye_yn" -> Array("1"),
+      "hs_dx_cat" -> Array("1"),
+      "hs_dx_cat_month" -> Array("2"),
+      "hs_dx_cat_year" -> Array("2020"),
+      "hs_dx_cat_surg" -> Array("3"),
+      "hs_dx_cat_fu" -> Array("1"),
+      "hs_dx_blind" -> Array("1"),
+      "hs_dx_blind_month" -> Array("5"),
+      "hs_dx_blind_year" -> Array("2020"),
+      "hs_dx_blind_surg" -> Array("4"),
+      "hs_dx_blind_fu" -> Array("0"),
+      "hs_dx_eye_cause_yn" -> Array("1"),
+      "hs_dx_eye_cause" -> Array("98"),
+      "hs_dx_eye_cause_other" -> Array("hummus"),
+      "hs_dx_eye_other" -> Array("1"),
+      "hs_dx_eye_other_spec" -> Array("falafel"),
+      "hs_dx_eye_other_month" -> Array("10"),
+      "hs_dx_eye_other_year" -> Array("2020"),
+      "hs_dx_eye_other_surg" -> Array("1"),
+      "hs_dx_eye_other_fu" -> Array("1")
+    )
     val exampleEyeDiseaseRecord = RawRecord(id = 1, multipleEyeDisease)
     val output = HealthTransformations.mapHealthConditions(exampleEyeDiseaseRecord)
 
@@ -328,6 +293,16 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map congenital eye disorders when values are defined for a single congenital eye disorder" in {
+    val singleCongenitalEyeDisorder = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_congenital_yn" -> Array("1"),
+      "hs_cg_disorders_yn" -> Array("1"),
+      "hs_cg_eye_cat" -> Array("1"),
+      "hs_cg_eye_cat_month" -> Array("2"),
+      "hs_cg_eye_cat_year" -> Array("2020"),
+      "hs_cg_eye_cat_surg" -> Array("3"),
+      "hs_cg_eye_cat_fu" -> Array("1")
+    )
     val exampleEyeDisorderRecord = RawRecord(id = 1, singleCongenitalEyeDisorder)
     val output = HealthTransformations.mapHealthConditions(exampleEyeDisorderRecord)
 
@@ -347,6 +322,22 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "correctly map congenital eye disorders when values are defined for multiple congenital eye disorders" in {
+    val multipleCongenitalEyeDisorder = Map[String, Array[String]](
+      "study_id" -> Array("10"),
+      "hs_congenital_yn" -> Array("1"),
+      "hs_cg_eye_disorders_yn" -> Array("1"),
+      "hs_cg_eye_cat" -> Array("1"),
+      "hs_cg_eye_cat_month" -> Array("2"),
+      "hs_cg_eye_cat_year" -> Array("2020"),
+      "hs_cg_eye_cat_surg" -> Array("3"),
+      "hs_cg_eye_cat_fu" -> Array("1"),
+      "hs_cg_eye_other" -> Array("1"),
+      "hs_cg_eye_other_spec" -> Array("olives"),
+      "hs_cg_eye_other_month" -> Array("2"),
+      "hs_cg_eye_other_year" -> Array("2020"),
+      "hs_cg_eye_other_surg" -> Array("3"),
+      "hs_cg_eye_other_fu" -> Array("0")
+    )
     val exampleEyeDisorderRecord = RawRecord(id = 1, multipleCongenitalEyeDisorder)
     val output = HealthTransformations.mapHealthConditions(exampleEyeDisorderRecord)
 
