5919e4dd8610e51f1b6a3c2a8630cdbb9edb34f3
Dan Moran <danmoran@broadinstitute.org>
2020-06-25 22:44:13 -0400

    Better!

--- hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HealthTransformations.scala
+++ hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/HealthTransformations.scala
@@ -8,76 +8,61 @@ object HealthTransformations {
   /** Parse all health-condition-related fields out of a raw RedCap record. */
   def mapHealthConditions(rawRecord: RawRecord): Iterable[HlesHealthCondition] =
     HealthCondition.values.flatMap { healthCondition =>
-      val cg = healthCondition.conditionType.cgKey.flatMap { cgKey =>
-        if (rawRecord.getBoolean("hs_congenital_yn") &&
-            rawRecord.getBoolean(cgKey.gate) &&
-            healthCondition.hasCg) {
-
-          val prefix = healthCondition.prefixOverride.getOrElse(
-            s"hs_cg_${cgKey.abbreviation}_${healthCondition.abbreviation}"
-          )
-
-          val conditionGate = healthCondition.computeGate(prefix)
-          if (rawRecord.getBoolean(conditionGate)) {
-            val base = createHealthConditionRow(
-              rawRecord,
-              prefix,
-              healthCondition.conditionType.value,
-              healthCondition.value,
-              isCongenital = true
-            )
-            Some {
-              if (healthCondition.isOther) {
-                base.copy(hsConditionOtherDescription = rawRecord.getOptional(s"${prefix}_spec"))
-              } else {
-                base
-              }
-            }
-          } else {
-            None
-          }
+      val cg = for {
+        cgKey <- healthCondition.conditionType.cgKey
+        if healthCondition.hasCg &&
+          rawRecord.getBoolean("hs_congenital_yn") &&
+          rawRecord.getBoolean(cgKey.gate)
+        prefix = healthCondition.prefixOverride.getOrElse(
+          s"hs_cg_${cgKey.abbreviation}_${healthCondition.abbreviation}"
+        )
+        conditionGate = healthCondition.computeGate(prefix)
+        if rawRecord.getBoolean(conditionGate)
+      } yield {
+        val base = createHealthConditionRow(
+          rawRecord,
+          prefix,
+          healthCondition.conditionType.value,
+          healthCondition.value,
+          isCongenital = true
+        )
+        if (healthCondition.isOther) {
+          base.copy(hsConditionOtherDescription = rawRecord.getOptional(s"${prefix}_spec"))
         } else {
-          None
+          base
         }
       }
 
-      val dx = healthCondition.conditionType.dxKey.flatMap { dxKey =>
-        if (rawRecord.getBoolean(dxKey.gate)) {
-          val infix =
-            if (dxKey.typePrefixed || healthCondition.isOther) s"_${dxKey.abbreviation}" else ""
-          val prefix = s"hs_dx${infix}_${healthCondition.abbreviation}"
-          if (rawRecord.getBoolean(prefix)) {
-            val base = createHealthConditionRow(
-              rawRecord,
-              prefix,
-              healthCondition.conditionType.value,
-              healthCondition.value,
-              isCongenital = false
-            )
-            Some {
-              if (healthCondition == HealthCondition.Blindness) {
-                val isCauseKnown = rawRecord.getBoolean("hs_dx_eye_cause_yn")
-                val conditionCause =
-                  if (isCauseKnown) rawRecord.getOptionalNumber("hs_dx_eye_cause") else None
-                base.copy(
-                  hsConditionCause = conditionCause,
-                  hsConditionCauseOtherDescription = if (conditionCause.contains(98)) {
-                    rawRecord.getOptional("hs_dx_eye_cause_other")
-                  } else {
-                    None
-                  }
-                )
-              } else if (healthCondition.isOther) {
-                base.copy(hsConditionOtherDescription = rawRecord.getOptional(s"${prefix}_spec"))
-              } else {
-                base
-              }
+      val dx = for {
+        dxKey <- healthCondition.conditionType.dxKey
+        if healthCondition.hasDx && rawRecord.getBoolean(dxKey.gate)
+        infix = if (dxKey.typePrefixed || healthCondition.isOther) s"_${dxKey.abbreviation}" else ""
+        prefix = s"hs_dx${infix}_${healthCondition.abbreviation}"
+        if rawRecord.getBoolean(prefix)
+      } yield {
+        val base = createHealthConditionRow(
+          rawRecord,
+          prefix,
+          healthCondition.conditionType.value,
+          healthCondition.value,
+          isCongenital = false
+        )
+        if (healthCondition == HealthCondition.Blindness) {
+          val isCauseKnown = rawRecord.getBoolean("hs_dx_eye_cause_yn")
+          val conditionCause =
+            if (isCauseKnown) rawRecord.getOptionalNumber("hs_dx_eye_cause") else None
+          base.copy(
+            hsConditionCause = conditionCause,
+            hsConditionCauseOtherDescription = if (conditionCause.contains(98)) {
+              rawRecord.getOptional("hs_dx_eye_cause_other")
+            } else {
+              None
             }
-          } else {
-            None
-          }
+          )
+        } else if (healthCondition.isOther) {
+          base.copy(hsConditionOtherDescription = rawRecord.getOptional(s"${prefix}_spec"))
         } else {
-          None
+          base
         }
       }
 
@@ -91,20 +76,19 @@ object HealthTransformations {
     conditionType: Long,
     condition: Long,
     isCongenital: Boolean
-  ): HlesHealthCondition =
-    HlesHealthCondition(
-      dogId = rawRecord.getRequired("study_id").toLong,
-      hsConditionType = conditionType,
-      hsCondition = condition,
-      hsConditionOtherDescription = None,
-      hsConditionIsCongenital = isCongenital,
-      hsConditionCause = None,
-      hsConditionCauseOtherDescription = None,
-      hsDiagnosisYear = rawRecord.getOptionalNumber(s"${fieldPrefix}_year"),
-      hsDiagnosisMonth = rawRecord.getOptionalNumber(s"${fieldPrefix}_month"),
-      hsRequiredSurgeryOrHospitalization = rawRecord.getOptionalNumber(s"${fieldPrefix}_surg"),
-      hsFollowUpOngoing = rawRecord.getOptionalBoolean(s"${fieldPrefix}_fu")
-    )
+  ): HlesHealthCondition = HlesHealthCondition(
+    dogId = rawRecord.getRequired("study_id").toLong,
+    hsConditionType = conditionType,
+    hsCondition = condition,
+    hsConditionOtherDescription = None,
+    hsConditionIsCongenital = isCongenital,
+    hsConditionCause = None,
+    hsConditionCauseOtherDescription = None,
+    hsDiagnosisYear = rawRecord.getOptionalNumber(s"${fieldPrefix}_year"),
+    hsDiagnosisMonth = rawRecord.getOptionalNumber(s"${fieldPrefix}_month"),
+    hsRequiredSurgeryOrHospitalization = rawRecord.getOptionalNumber(s"${fieldPrefix}_surg"),
+    hsFollowUpOngoing = rawRecord.getOptionalBoolean(s"${fieldPrefix}_fu")
+  )
   /*
   val healthConditions: List[HealthCondition] = List(
     // infectious conditions
