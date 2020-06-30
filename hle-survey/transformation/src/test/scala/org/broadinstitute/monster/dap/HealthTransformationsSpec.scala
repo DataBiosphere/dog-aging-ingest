c7d3fea01bff23f1562e7a8ebd9839888a347ca8
raaidbroad <52425631+raaidbroad@users.noreply.github.com>
2020-06-30 14:19:30 -0400

    ra-DSPDC-866-health-skin (#43)
    * add skin conditions and tests
    
    * fix long values for skin conditions

--- hle-survey/transformation/src/test/scala/org/broadinstitute/monster/dap/HealthTransformationsSpec.scala
+++ hle-survey/transformation/src/test/scala/org/broadinstitute/monster/dap/HealthTransformationsSpec.scala
@@ -579,6 +579,124 @@ class HealthTransformationsSpec extends AnyFlatSpec with Matchers {
     output should contain theSameElementsAs truth
   }
 
+  it should "correctly map congenital skin disorders when values are defined" in {
+    val multipleCongenitalSkinDisorder = Map[String, Array[String]](
+      "hs_congenital_yn" -> Array("1"),
+      "hs_cg_skin_disorders_yn" -> Array("1"),
+      "hs_cg_skin_dcysts" -> Array("1"),
+      "hs_cg_skin_dcysts_month" -> Array("2"),
+      "hs_cg_skin_dcysts_year" -> Array("2020"),
+      "hs_cg_skin_dcysts_surg" -> Array("3"),
+      "hs_cg_skin_dcysts_fu" -> Array("1"),
+      "hs_cg_skin_other" -> Array("1"),
+      "hs_cg_skin_other_spec" -> Array("olives"),
+      "hs_cg_skin_other_month" -> Array("2"),
+      "hs_cg_skin_other_year" -> Array("2020"),
+      "hs_cg_skin_other_surg" -> Array("3"),
+      "hs_cg_skin_other_fu" -> Array("0")
+    )
+    val exampleSkinDisorderRecord = RawRecord(id = 1, multipleCongenitalSkinDisorder)
+    val output = HealthTransformations.mapHealthConditions(exampleSkinDisorderRecord)
+
+    val truth = List(
+      HlesHealthCondition(
+        dogId = 1L,
+        hsConditionType = HealthConditionType.Skin.value,
+        hsCondition = HealthCondition.DermoidCysts.value,
+        hsConditionOtherDescription = None,
+        hsConditionIsCongenital = true,
+        hsConditionCause = None,
+        hsConditionCauseOtherDescription = None,
+        hsDiagnosisYear = Some(2020),
+        hsDiagnosisMonth = Some(2),
+        hsRequiredSurgeryOrHospitalization = Some(3),
+        hsFollowUpOngoing = Some(true)
+      ),
+      HlesHealthCondition(
+        dogId = 1L,
+        hsConditionType = HealthConditionType.Skin.value,
+        hsCondition = HealthCondition.OtherSkin.value,
+        hsConditionOtherDescription = Some("olives"),
+        hsConditionIsCongenital = true,
+        hsConditionCause = None,
+        hsConditionCauseOtherDescription = None,
+        hsDiagnosisYear = Some(2020),
+        hsDiagnosisMonth = Some(2),
+        hsRequiredSurgeryOrHospitalization = Some(3),
+        hsFollowUpOngoing = Some(false)
+      )
+    )
+
+    output should contain theSameElementsAs (truth)
+  }
+
+  it should "correctly map skin disease values when values are defined" in {
+    val multipleEarDisease = Map[String, Array[String]](
+      "hs_dx_skin_yn" -> Array("1"),
+      "hs_dx_skin_alo" -> Array("1"),
+      "hs_dx_skin_alo_month" -> Array("2"),
+      "hs_dx_skin_alo_year" -> Array("2020"),
+      "hs_dx_skin_alo_surg" -> Array("3"),
+      "hs_dx_skin_alo_fu" -> Array("1"),
+      "hs_dx_skin_cd" -> Array("1"),
+      "hs_dx_skin_cd_month" -> Array("2"),
+      "hs_dx_skin_cd_year" -> Array("2020"),
+      "hs_dx_skin_cd_surg" -> Array("3"),
+      "hs_dx_skin_cd_fu" -> Array("1"),
+      "hs_dx_skin_other" -> Array("1"),
+      "hs_dx_skin_other_spec" -> Array("ohno"),
+      "hs_dx_skin_other_month" -> Array("2"),
+      "hs_dx_skin_other_year" -> Array("2020"),
+      "hs_dx_skin_other_surg" -> Array("3"),
+      "hs_dx_skin_other_fu" -> Array("1")
+    )
+    val exampleEarDiseaseRecord = RawRecord(id = 1, multipleEarDisease)
+    val output = HealthTransformations.mapHealthConditions(exampleEarDiseaseRecord)
+    val truth = List(
+      HlesHealthCondition(
+        dogId = 1L,
+        hsConditionType = HealthConditionType.Skin.value,
+        hsCondition = HealthCondition.Alopecia.value,
+        hsConditionOtherDescription = None,
+        hsConditionIsCongenital = false,
+        hsConditionCause = None,
+        hsConditionCauseOtherDescription = None,
+        hsDiagnosisYear = Some(2020),
+        hsDiagnosisMonth = Some(2),
+        hsRequiredSurgeryOrHospitalization = Some(3),
+        hsFollowUpOngoing = Some(true)
+      ),
+      HlesHealthCondition(
+        dogId = 1L,
+        hsConditionType = HealthConditionType.Skin.value,
+        hsCondition = HealthCondition.ContactDermatitis.value,
+        hsConditionOtherDescription = None,
+        hsConditionIsCongenital = false,
+        hsConditionCause = None,
+        hsConditionCauseOtherDescription = None,
+        hsDiagnosisYear = Some(2020),
+        hsDiagnosisMonth = Some(2),
+        hsRequiredSurgeryOrHospitalization = Some(3),
+        hsFollowUpOngoing = Some(true)
+      ),
+      HlesHealthCondition(
+        dogId = 1L,
+        hsConditionType = HealthConditionType.Skin.value,
+        hsCondition = HealthCondition.OtherSkin.value,
+        hsConditionOtherDescription = Some("ohno"),
+        hsConditionIsCongenital = false,
+        hsConditionCause = None,
+        hsConditionCauseOtherDescription = None,
+        hsDiagnosisYear = Some(2020),
+        hsDiagnosisMonth = Some(2),
+        hsRequiredSurgeryOrHospitalization = Some(3),
+        hsFollowUpOngoing = Some(true)
+      )
+    )
+
+    output should contain theSameElementsAs truth
+  }
+
   it should "correctly map health status data when fields are null" in {
     val emptyRecord = RawRecord(1, Map.empty)
 
