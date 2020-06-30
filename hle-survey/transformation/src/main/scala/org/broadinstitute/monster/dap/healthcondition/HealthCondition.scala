3a9e95d1ddba1f0f3659ed22a6832b1a2ce1480f
raaidbroad <52425631+raaidbroad@users.noreply.github.com>
2020-06-30 13:16:59 -0400

    Ra dspdc 881 health trauma (#42)
    * add ear conditions and tests
    
    * add trauma conditions and tests

--- hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/healthcondition/HealthCondition.scala
+++ hle-survey/transformation/src/main/scala/org/broadinstitute/monster/dap/healthcondition/HealthCondition.scala
@@ -63,7 +63,16 @@ object HealthCondition extends LongEnum[HealthCondition] {
   case object OtherEye extends HealthCondition(198L, "Other eye condition", Eye, "eye_other", true, true, true, Some("hs_cg_eye_other"))
 
   // Ear conditions.
-  // TODO
+  case object Deafness extends HealthCondition(201L, "Deafness", Ear, "deaf", true, true)
+  case object EarInfection extends HealthCondition(202L, "Ear Infection", Ear, "ei", false, true)
+  case object EarMites extends HealthCondition(203L, "Ear Mites", Ear, "em", false, true)
+  case object Epistaxis extends HealthCondition(204L, "Epistaxis (nose bleeds)", Ear, "epis", false, true)
+  case object HearingLoss extends HealthCondition(205L, "Hearing loss (incompletely deaf)", Ear, "hl", false, true)
+  case object Hematoma extends HealthCondition(206L, "Hematoma", Ear, "hemato", false, true)
+  case object Pharyngitis extends HealthCondition(207L, "Pharyngitis", Ear, "phary", false, true)
+  case object Rhinitis extends HealthCondition(208L, "Rhinitis", Ear, "rhini", false, true)
+  case object Tonsillitis extends HealthCondition(209L, "Tonsillitis", Ear, "tonsi", false, true)
+  case object OtherEar extends HealthCondition(298L, "Other ear condition", Ear, "other", true, true, true, Some("hs_cg_ear_other"))
 
   // Oral conditions.
   // TODO
@@ -164,7 +173,20 @@ object HealthCondition extends LongEnum[HealthCondition] {
   // TODO
 
   // Trauma.
-  // TODO.
+  case object DogBite extends HealthCondition(1801L, "Dog bite", Trauma, "dogbite", false, true)
+  case object AnimalBite extends HealthCondition(1802L, "Bite wound from another animal", Trauma, "anibite", false, true)
+  case object Fall extends HealthCondition(1803L, "Fall from height", Trauma, "fall", false, true)
+  case object Fracture extends HealthCondition(1804L, "Fractured bone", Trauma, "frac", false, true)
+  case object Head extends HealthCondition(1805L, "Head trauma due to any cause", Trauma, "head", false, true)
+  case object Car extends HealthCondition(1806L, "Hit by car or other vehicle", Trauma, "car", false, true)
+  case object Kick extends HealthCondition(1807L, "Kicked by horse or other large animal", Trauma, "kick", false, true)
+  case object Laceration extends HealthCondition(1808L, "Laceration", Trauma, "lac", false, true)
+  case object PenetratingWound extends HealthCondition(1809L, "Penetrating wound (such as a stick)", Trauma, "pene", false, true)
+  case object Proptosis extends HealthCondition(1810L, "Proptosis (eye out of socket)", Trauma, "prop", false, true)
+  case object SnakeBite extends HealthCondition(1811L, "Snakebite", Trauma, "snake", false, true)
+  case object Tail extends HealthCondition(1812L, "Tail injury", Trauma, "tail", false, true)
+  case object Nail extends HealthCondition(1813L, "Torn or broken toenail", Trauma, "nail", false, true)
+  case object OtherTrauma extends HealthCondition(1898L, "Other trauma", Trauma, "other", false, true, true)
 
   // Immune conditions.
   // TODO
