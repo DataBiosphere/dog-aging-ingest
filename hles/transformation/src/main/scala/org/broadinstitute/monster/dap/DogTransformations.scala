3db67081df9bc72bcfb705f6bdae655a56e17426
Quazi <59845076+quazi-broad@users.noreply.github.com>
2020-10-30 14:14:26 -0400

    Adding error handling capability (implemented specifically for missing owner_id) (#66)
    * Bumping ingest-sbt-plugins to 2.1.3
    Added a new HLESurveyTransformationLog file with a general HLESurveyTransformationError class
    Added a case class for MissingOwnerIdError, we can add additional validations as needed
    Added an error logger to the logback file
    Added a PostProcess object to the Pipeline
    Added an implicit logger within PipelineBuilder to help with association
    Added logic to filter and catch records that are missing st_owner_id using the MissingOwnerIdError
    Updated Dog and Owner Transformation with new logic and added unit test cases for missing st_owner_id

--- hles/transformation/src/main/scala/org/broadinstitute/monster/dap/DogTransformations.scala
+++ hles/transformation/src/main/scala/org/broadinstitute/monster/dap/DogTransformations.scala
@@ -2,28 +2,40 @@ package org.broadinstitute.monster.dap
 
 import org.broadinstitute.monster.dap.dog._
 import org.broadinstitute.monster.dogaging.jadeschema.table.HlesDog
+import org.broadinstitute.monster.dap.HLESurveyTransformationPipelineBuilder.logger
 
 object DogTransformations {
 
   /** Parse all dog-related fields out of a raw RedCap record. */
-  def mapDog(rawRecord: RawRecord): HlesDog =
-    HlesDog(
-      dogId = rawRecord.id,
-      ownerId = rawRecord.getRequired("st_owner_id").toLong,
-      hlesDogStudyStatus = Some(StudyStatusTransformations.mapStudyStatus(rawRecord)),
-      hlesDogDemographics = Some(DemographicsTransformations.mapDemographics(rawRecord)),
-      hlesDogResidences = Some(DogResidenceTransformations.mapDogResidences(rawRecord)),
-      hlesDogPhysicalActivity =
-        Some(PhysicalActivityTransformations.mapPhysicalActivity(rawRecord)),
-      hlesDogResidentialEnvironment =
-        Some(ResidentialEnvironmentTransformations.mapResidentialEnvironment(rawRecord)),
-      hlesDogRoutineEnvironment =
-        Some(RoutineEnvironmentTransformations.mapRoutineEnvironment(rawRecord)),
-      hlesDogBehavior = Some(BehaviorTransformations.mapBehavior(rawRecord)),
-      hlesDogDiet = Some(DietTransformations.mapDiet(rawRecord)),
-      hlesDogMedsPreventatives =
-        Some(MedsAndPreventativesTransformations.mapMedsPreventatives(rawRecord)),
-      hlesDogHealthSummary = Some(HealthStatusTransformations.mapHealthSummary(rawRecord)),
-      hlesDogFutureStudies = Some(AdditionalStudiesTransformations.mapFutureStudies(rawRecord))
-    )
+  def mapDog(rawRecord: RawRecord): Option[HlesDog] = {
+    val dog_id = rawRecord.id
+
+    rawRecord.getOptional("st_owner_id") match {
+      case None =>
+        MissingOwnerIdError(s"Record $dog_id has more/less than 1 value for field st_owner_id").log
+        None
+      case Some(owner_id) =>
+        Some(
+          HlesDog(
+            dogId = dog_id,
+            ownerId = owner_id.toLong,
+            hlesDogStudyStatus = Some(StudyStatusTransformations.mapStudyStatus(rawRecord)),
+            hlesDogDemographics = Some(DemographicsTransformations.mapDemographics(rawRecord)),
+            hlesDogResidences = Some(DogResidenceTransformations.mapDogResidences(rawRecord)),
+            hlesDogPhysicalActivity =
+              Some(PhysicalActivityTransformations.mapPhysicalActivity(rawRecord)),
+            hlesDogResidentialEnvironment =
+              Some(ResidentialEnvironmentTransformations.mapResidentialEnvironment(rawRecord)),
+            hlesDogRoutineEnvironment =
+              Some(RoutineEnvironmentTransformations.mapRoutineEnvironment(rawRecord)),
+            hlesDogBehavior = Some(BehaviorTransformations.mapBehavior(rawRecord)),
+            hlesDogDiet = Some(DietTransformations.mapDiet(rawRecord)),
+            hlesDogMedsPreventatives =
+              Some(MedsAndPreventativesTransformations.mapMedsPreventatives(rawRecord)),
+            hlesDogHealthSummary = Some(HealthStatusTransformations.mapHealthSummary(rawRecord)),
+            hlesDogFutureStudies = Some(AdditionalStudiesTransformations.mapFutureStudies(rawRecord))
+          )
+        )
+    }
+  }
 }
--- hles/transformation/src/main/scala/org/broadinstitute/monster/dap/OwnerTransformations.scala
+++ hles/transformation/src/main/scala/org/broadinstitute/monster/dap/OwnerTransformations.scala
@@ -2,6 +2,7 @@ package org.broadinstitute.monster.dap
 
 import org.broadinstitute.monster.dogaging.jadeschema.table.HlesOwner
 import scala.util.matching.Regex
+import org.broadinstitute.monster.dap.HLESurveyTransformationPipelineBuilder.logger
 
 object OwnerTransformations {
 
@@ -21,7 +22,8 @@ object OwnerTransformations {
       .map(_.group(1).toLong)
 
   /** Parse all owner-related fields out of a raw RedCap record. */
-  def mapOwner(rawRecord: RawRecord): HlesOwner = {
+  def mapOwner(rawRecord: RawRecord): Option[HlesOwner] = {
+    val dog_id = rawRecord.id
     val raceValues = rawRecord.fields.get("od_race")
     val otherRace = raceValues.map(_.contains("98"))
 
@@ -31,51 +33,62 @@ object OwnerTransformations {
       if (_) rawRecord.getOptionalNumber("oc_address2_own") else None
     }
 
-    HlesOwner(
-      ownerId = rawRecord.getRequired("st_owner_id").toLong,
-      odAgeRangeYears = rawRecord.getOptionalNumber("od_age"),
-      odMaxEducation = rawRecord.getOptionalNumber("od_education"),
-      odRaceWhite = raceValues.map(_.contains("1")),
-      odRaceBlackOrAfricanAmerican = raceValues.map(_.contains("2")),
-      odRaceAsian = raceValues.map(_.contains("3")),
-      odRaceAmericanIndian = raceValues.map(_.contains("4")),
-      odRaceAlaskaNative = raceValues.map(_.contains("5")),
-      odRaceNativeHawaiian = raceValues.map(_.contains("6")),
-      odRaceOtherPacificIslander = raceValues.map(_.contains("7")),
-      odRaceOther = otherRace,
-      odRaceOtherDescription = otherRace.flatMap {
-        if (_) rawRecord.getOptional("od_race_other") else None
-      },
-      odHispanic = rawRecord.getOptionalBoolean("od_hispanic_yn"),
-      odAnnualIncomeRangeUsd = rawRecord.getOptionalNumber("od_income"),
-      ocHouseholdPersonCount = rawRecord.getOptionalNumber("oc_people_household"),
-      ocHouseholdAdultCount = rawRecord.getOptionalNumber("oc_adults_household"),
-      ocHouseholdChildCount = rawRecord.getOptionalNumber("oc_children_household"),
-      ssHouseholdDogCount = rawRecord.getOptionalNumber("ss_num_dogs_hh"),
-      ocPrimaryResidenceState = rawRecord.getOptional("oc_address1_state"),
-      ocPrimaryResidenceCensusDivision = rawRecord.getOptional("oc_address1_division").flatMap {
-        getCensusDivision(_)
-      },
-      ocPrimaryResidenceZip = rawRecord.getOptional("oc_address1_zip"),
-      ocPrimaryResidenceOwnership = primaryAddressOwnership,
-      ocPrimaryResidenceOwnershipOtherDescription = if (primaryAddressOwnership.contains(98)) {
-        rawRecord.getOptional("oc_address1_own_other")
-      } else {
+    rawRecord.getOptional("st_owner_id") match {
+      case None =>
+        MissingOwnerIdError(s"Record $dog_id has less than 1 value for field st_owner_id").log
         None
-      },
-      ocSecondaryResidence = secondaryAddress,
-      ocSecondaryResidenceState = secondaryAddress.flatMap {
-        if (_) rawRecord.getOptional("oc_address2_state") else None
-      },
-      ocSecondaryResidenceZip = secondaryAddress.flatMap {
-        if (_) rawRecord.getOptional("oc_address2_zip") else None
-      },
-      ocSecondaryResidenceOwnership = secondaryAddressOwnership,
-      ocSecondaryResidenceOwnershipOtherDescription = if (secondaryAddressOwnership.contains(98)) {
-        rawRecord.getOptional("oc_address2_own_other")
-      } else {
-        None
-      }
-    )
+      case Some(owner_id) =>
+        Some(
+          HlesOwner(
+            ownerId = owner_id.toLong,
+            odAgeRangeYears = rawRecord.getOptionalNumber("od_age"),
+            odMaxEducation = rawRecord.getOptionalNumber("od_education"),
+            odRaceWhite = raceValues.map(_.contains("1")),
+            odRaceBlackOrAfricanAmerican = raceValues.map(_.contains("2")),
+            odRaceAsian = raceValues.map(_.contains("3")),
+            odRaceAmericanIndian = raceValues.map(_.contains("4")),
+            odRaceAlaskaNative = raceValues.map(_.contains("5")),
+            odRaceNativeHawaiian = raceValues.map(_.contains("6")),
+            odRaceOtherPacificIslander = raceValues.map(_.contains("7")),
+            odRaceOther = otherRace,
+            odRaceOtherDescription = otherRace.flatMap {
+              if (_) rawRecord.getOptional("od_race_other") else None
+            },
+            odHispanic = rawRecord.getOptionalBoolean("od_hispanic_yn"),
+            odAnnualIncomeRangeUsd = rawRecord.getOptionalNumber("od_income"),
+            ocHouseholdPersonCount = rawRecord.getOptionalNumber("oc_people_household"),
+            ocHouseholdAdultCount = rawRecord.getOptionalNumber("oc_adults_household"),
+            ocHouseholdChildCount = rawRecord.getOptionalNumber("oc_children_household"),
+            ssHouseholdDogCount = rawRecord.getOptionalNumber("ss_num_dogs_hh"),
+            ocPrimaryResidenceState = rawRecord.getOptional("oc_address1_state"),
+            ocPrimaryResidenceCensusDivision =
+              rawRecord.getOptional("oc_address1_division").flatMap {
+                getCensusDivision(_)
+              },
+            ocPrimaryResidenceZip = rawRecord.getOptional("oc_address1_zip"),
+            ocPrimaryResidenceOwnership = primaryAddressOwnership,
+            ocPrimaryResidenceOwnershipOtherDescription =
+              if (primaryAddressOwnership.contains(98)) {
+                rawRecord.getOptional("oc_address1_own_other")
+              } else {
+                None
+              },
+            ocSecondaryResidence = secondaryAddress,
+            ocSecondaryResidenceState = secondaryAddress.flatMap {
+              if (_) rawRecord.getOptional("oc_address2_state") else None
+            },
+            ocSecondaryResidenceZip = secondaryAddress.flatMap {
+              if (_) rawRecord.getOptional("oc_address2_zip") else None
+            },
+            ocSecondaryResidenceOwnership = secondaryAddressOwnership,
+            ocSecondaryResidenceOwnershipOtherDescription =
+              if (secondaryAddressOwnership.contains(98)) {
+                rawRecord.getOptional("oc_address2_own_other")
+              } else {
+                None
+              }
+          )
+        )
+    }
   }
 }
