ff788d5fb7bd3c324a469b1fc44be776232249a6
Steve Pletcher <spletche@broadinstitute.org>
2020-12-03 13:31:08 -0500

    CR response, reorganize tests

--- hles/transformation/src/test/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineValidationSpec.scala
+++ hles/transformation/src/test/scala/org/broadinstitute/monster/dap/HLESurveyTransformationPipelineValidationSpec.scala
@@ -1,16 +1,17 @@
 package org.broadinstitute.monster.dap
 
 import com.spotify.scio.testing.PipelineSpec
+import com.spotify.scio.ScioResult
 import org.apache.beam.sdk.options.{PipelineOptions, PipelineOptionsFactory}
 import org.broadinstitute.monster.common.PipelineCoders
 import org.broadinstitute.monster.common.msg.JsonParser
 import org.broadinstitute.monster.dap.PostProcess.errCount
-import org.scalatest
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import better.files.File
 import org.apache.beam.sdk.metrics.MetricName
+import upack.Msg
 
 import scala.io.Source
 
@@ -23,17 +24,16 @@ class HLESurveyTransformationPipelineValidationSpec
   behavior of "HLESurveyTransformationPipeline"
 
   val opts: PipelineOptions = PipelineOptionsFactory.fromArgs("--runner=DirectRunner").create()
+  val errorCountMetric: MetricName = MetricName.named("main", errCount)
 
   // set up a pipeline output directory that we can then clean up
   val outputDir = File.newTemporaryDirectory()
   override def afterAll(): Unit = outputDir.delete()
 
   def pipelineTest(
-    inputPath: String,
-    expectedErrorMessage: String,
-    count: Int = 1
-  ): scalatest.Assertion = {
-    val result = runWithRealContext(opts)(ctx =>
+    inputPath: String
+  ): ScioResult = {
+    runWithRealContext(opts)(ctx =>
       HLESurveyTransformationPipelineBuilder.buildPipeline(
         ctx,
         Args(
@@ -41,34 +41,40 @@ class HLESurveyTransformationPipelineValidationSpec
           outputDir.pathAsString
         )
       )
-    ).waitUntilFinish()
-    val expectedErrorMessageJson = JsonParser.parseEncodedJson(expectedErrorMessage)
-
-    // make sure logs are correct
-    val src = Source.fromFile("../logs/errors.log")
-    val lines = src.getLines.toList
-    src.close()
-    lines.map(err => JsonParser.parseEncodedJson(err)).takeRight(count) should contain(
-      expectedErrorMessageJson
-    )
-
-    // make sure counter was incremented right number of times
-    val errorCountMetric: MetricName = MetricName.named("main", errCount)
-    assert(result.allCounters.isDefinedAt(errorCountMetric))
-    result.allCounters.foreach {
-      case (name, errorCount) =>
-        if (name == errorCountMetric) errorCount.attempted shouldBe count
-    }
-
-    assertThrows[HLESurveyTransformationFailException](PostProcess.postProcess(result))
+    ).waitUntilDone()
   }
 
-  it should "log an error in each place where st_owner_id is None" in {
-    val expected =
+  def getParsedErrorJsonFromLog(
+    logPath: String,
+    expectedErrorCount: Int
+  ): Iterable[Msg] = {
+    val src = Source.fromFile(logPath)
+    val lines = src.getLines.toList
+    src.close()
+    lines.map(err => JsonParser.parseEncodedJson(err)).takeRight(expectedErrorCount)
+  }
+
+  it should "produce one error for each missing st_owner_id" in {
+    val result = pipelineTest("blank_st_owner_id")
+    result.allCounters(errorCountMetric).attempted shouldBe 2
+  }
+
+  it should "produce the expected error message for a missing st_owner_id" in {
+    val expectedRawJson =
       """
         |{"errorType":"MissingOwnerIdError",
-        |"message":"Record 87616 has less than 1 value for field st_owner_id"}
+        |"message":"Record 11111 has less than 1 value for field st_owner_id"}
         |""".stripMargin
-    pipelineTest("blank_st_owner_id", expected, 2)
+    val expectedParsedJson = JsonParser.parseEncodedJson(expectedRawJson)
+
+    pipelineTest("blank_st_owner_id")
+    val loggedErrorJson = getParsedErrorJsonFromLog("../logs/errors.log", 2)
+
+    loggedErrorJson should contain(expectedParsedJson)
+  }
+
+  it should "fail the pipeline when there is a missing st_owner_id" in {
+    val result = pipelineTest("blank_st_owner_id")
+    an[HLESurveyTransformationFailException] should be thrownBy PostProcess.postProcess(result)
   }
 }
