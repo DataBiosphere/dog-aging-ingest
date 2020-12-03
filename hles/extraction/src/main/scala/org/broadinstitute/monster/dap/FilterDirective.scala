e05043eba6773c2dc83b93f509aac7a5f03c8846
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2020-12-03 13:06:47 -0500

    [DSPDC-1331] Implement DAP pack filtering (#77)
    Why
    
    Relevant ticket
    
    We want to filter for
    
    the presence of st_dap_pack_count
    the presence of st_dap_pack_date
    In my testing, the REDCap API does not support "not null" conditions, so I've implemented something more hacky where we check for st_dap_pack_count > 0 and st_dap_pack_date > some magic date in the past.
    
    This PR
    
    We had hardcoded our filtering logic in the redcap client to only support =. I've extracted the filtering directives to their own typesafe class so we can now support other operands (">" for now).
    Updates the CSLB + HLES pipelines with the new filtering approach, and adds the DAP pack filters to the HLES pipeline.

---
+++ hles/extraction/src/main/scala/org/broadinstitute/monster/dap/FilterDirective.scala
@@ -0,0 +1,22 @@
+package org.broadinstitute.monster.dap
+
+sealed trait FilterOp {
+  def op: String
+}
+
+object FilterOps {
+
+  case object == extends FilterOp {
+    def op: String = "="
+  }
+
+  case object > extends FilterOp {
+    def op: String = ">"
+  }
+}
+
+/**
+  * Represents a typesafe REDCap filterLogic directive.
+  * Their API expects directives to be of the form [field][op][value]
+  */
+case class FilterDirective(field: String, operation: FilterOp, comparand: String)
