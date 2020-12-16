99bffb9d1ad47b96b2d97786c26d3a565a8a9adb
Quazi Hoque <qhoque@broadinstitute.org>
2020-12-16 10:06:34 -0500

    "Updated hardcoded list of arms"

--- hles/extraction/src/main/scala/org/broadinstitute/monster/dap/EnvironmentExtractionPipeline.scala
+++ hles/extraction/src/main/scala/org/broadinstitute/monster/dap/EnvironmentExtractionPipeline.scala
@@ -22,9 +22,26 @@ object EnvironmentExtractionPipeline extends ScioApp[Args] {
   )
 
   val subdir = "environment"
+
   //todo: need to query for all arms and work through arms serially
   val arm =
-    List("baseline_arm_1", "dec2019_arm_1", "jan2020_arm_1")
+    List(
+      "baseline_arm_1",
+      "dec2019_arm_1",
+      "dec2019_secondary_arm_1",
+      "jan2020_arm_1",
+      "jan2020_secondary_arm_1",
+      "feb2020_arm_1",
+      "feb2020_secondary_arm_1",
+      "mar2020_arm_1",
+      "mar2020_secondary_arm_1",
+      "apr2020_arm_1",
+      "apr2020_secondary_arm_1",
+      "may2020_arm_1",
+      "may2020_secondary_arm_1",
+      "june2020_arm_1",
+      "june2020_secondary_arm_1"
+    )
   val fieldList = List("baseline_complete")
 
   override def pipelineBuilder: PipelineBuilder[Args] =
