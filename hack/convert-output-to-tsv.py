8ecd54c7a056fe0ea1e195048ca23aecd4249e47
Steve Pletcher <spletche@broadinstitute.org>
2021-01-08 13:42:48 -0500

    More tsv script improvements, finalize manual refresh script

--- hack/convert-output-to-tsv.py
+++ hack/convert-output-to-tsv.py
@@ -12,6 +12,7 @@ from gcsfs.core import GCSFileSystem
 parser = argparse.ArgumentParser(description="Convert the records in a specified bucket prefix to a Terra-compatible TSV")
 parser.add_argument('input_dir', metavar='I', help='The bucket prefix to read records from')
 parser.add_argument('output_dir', metavar='O', help='The local directory to write the resulting TSVs to')
+parser.add_argument('table', nargs='*', help="One or more tables to process into TSVs. Processes all tables if none specified.")
 parser.add_argument('--debug', action='store_true', help="Write additional logs for debugging")
 parser.parse_args()
 
@@ -19,7 +20,7 @@ log = logging.getLogger(__name__)
 
 TERRA_COLUMN_LIMIT = 512
 
-table_names = ['cslb', 'hles_cancer_condition', 'hles_dog', 'hles_health_condition', 'hles_owner']
+table_names = parser.table or ['cslb', 'hles_cancer_condition', 'hles_dog', 'hles_health_condition', 'hles_owner', 'environment']
 PRIMARY_KEY_PREFIX = 'entity'
 
 gcs = GCSFileSystem()
@@ -38,39 +39,37 @@ for table_name in table_names:
     # set to hold all columns for this table, list to hold all the rows
     column_set = set()
     row_list = []
+
+    if table_name in {"hles_owner", "hles_dog"}:
+        pk_name = remove_prefix(table_name, 'hles_') + "_id"
+        entity_name = f"{PRIMARY_KEY_PREFIX}:{pk_name}_id"
+    # cslb
+    elif table_name == 'cslb':
+        pk_name = 'dog_id'
+        entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
+    # hles_cancer_condition: read dog_id, copy and write out as pk_name
+    elif table_name == "hles_cancer_condition":
+        pk_name = 'dog_id'
+        entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
+    # hles_health_condition: read + copy dog_id and hs_condition and hs_condition_is_congenital
+    # concatenate and write out as pk_name
+    elif table_name == "hles_health_condition":
+        pk_name = None
+        entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
+    else:
+        log.info(f"Unrecognized table: {table_name}")
+        break
+
     # read json data
     for path in gcs.ls(os.path.join(parser.input_dir, table_name)):
         log.info(f"...Opening {path}")
+
         with gcs.open(path, 'r') as json_file:
             for line in json_file:
                 row = json.loads(line)
-                # rename primary key column to the format 'entity:{entity name}_id'
-                # pop existing primary keys out and push them back in
-                if table_name in {"hles_owner", "hles_dog"}:
-                    pk_name = remove_prefix(table_name, 'hles_')
-                    entity_name = f"{PRIMARY_KEY_PREFIX}:{pk_name}_id"
-                    row[entity_name] = row.pop(pk_name)
-                    # store data
-                    column_set.update(row.keys())
-                    row_list.append(row)
-                # cslb
-                elif table_name == 'cslb':
-                    entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
-                    row[entity_name] = row.get('dog_id')
-                    column_set.update(row.keys())
-                    row_list.append(row)
-                # hles_cancer_condition: read dog_id, copy and write out as pk_name
-                elif table_name == "hles_cancer_condition":
-                    entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
-                    # copy the dog_id
-                    row[entity_name] = row.get('dog_id')
-                    # store data
-                    column_set.update(row.keys())
-                    row_list.append(row)
                 # hles_health_condition: read + copy dog_id and hs_condition and hs_condition_is_congenital
                 # concatenate and write out as pk_name
-                elif table_name == "hles_health_condition":
-                    entity_name = f"{PRIMARY_KEY_PREFIX}:{table_name}_id"
+                if table_name == "hles_health_condition":
                     # grab the congenital flag and convert to int
                     congenital_flag = row.get('hs_condition_is_congenital')
                     try:
@@ -78,28 +77,16 @@ for table_name in table_names:
                     except TypeError:
                         log.info(f"Error, 'hs_condition_is_congenital' is not populated in {table_name}")
                     row[entity_name] = '-'.join([row.get('dog_id'), row.get('hs_condition'), congenital_flag])
-                    # store data
-                    column_set.update(row.keys())
-                    row_list.append(row)
-                # environment:
-                elif table_name == "environment":
-                    entity_name = pk_prefix + table_name + '_id'
-                    # env records are unique on dog_id, redcap_event
-                    # setting the primary key
-                    dogId = row.get('dog_id')
-                    eventName = row.get('address_month_year')
-                    row[entity_name] = ('%s-%s' %
-                        (dogId, eventName))
-                    column_set.update(row.keys())
-                    row_list.append(row)
                 else:
-                    log.info(f"Unrecognized table: {table_name}")
+                    row[entity_name] = row.pop(pk_name)
+
+                column_set.update(row.keys())
+                row_list.append(row)
 
     # make sure pk is the first column
     # pop out the PK, will be splitting this set out later
     column_set.remove(entity_name)
-    sorted_column_set = sorted(list(column_set))
-    sorted_column_set.insert(0, entity_name)
+    sorted_column_set = [entity_name] + sorted(list(column_set))
 
     # provide some stats
     col_count = len(sorted_column_set)
@@ -119,7 +106,7 @@ for table_name in table_names:
             log.info(f"...Processing Split #{chunk} to {output_location}")
             split_column_set = set()
             # add 511 columns
-            for _ in range(0, TERRA_COLUMN_LIMIT - 1):
+            for _ in range(TERRA_COLUMN_LIMIT - 1):
                 col = column_set.pop()
                 split_column_set.add(col)
                 log.debug(f"......adding {col} to split_column_set ({len(split_column_set)}) ...{len(column_set)} columns left")
@@ -134,7 +121,7 @@ for table_name in table_names:
                 except AttributeError:
                     return val
 
-            # iterate through every row looking for every column for this split
+            # iterate through every row slicing out the values for the columns in this split
             split_row_dict_list = [
                 {
                     col: clean_up_string_whitespace(row[col])
@@ -149,6 +136,7 @@ for table_name in table_names:
                 dw = csv.DictWriter(output_file, split_column_list, delimiter='\t')
                 dw.writeheader()
                 dw.writerows(split_row_dict_list)
+
             log.info(f"......{table_name} Split #{chunk} was successfully written to {output_location}")
     else:
         # this branch is executed for any tables with 512 cols or less
--- hack/manual_refresh.sh
+++ hack/manual_refresh.sh
@@ -2,44 +2,44 @@
 set -e
 
 # This script performs a manual refresh of our various DAP tables.
+#   Run this script from the root directory of your broadinstitute/dog-aging-ingest checkout.
 # Requirements:
-# * You must be authenticated with vault (you can check by running `vault token lookup` and seeing if your access info is displayed)
-# * You must be authenticated with GCP (`gcloud auth list` should show a star next to your email address)
-#   * Your account must be set as the default application credentials for your machine (you can do this with `gcloud auth application-default login`)
+#   You must have git read access to broadinstitute/terra-tools
+#   You must be authenticated with vault and have the vault CLI installed (you can check by running `vault token lookup` and seeing if your access info is displayed)
+#   You must be authenticated with GCP (`gcloud auth list` should show a star next to your email address)
+#     Your account must be set as the default application credentials for your machine (you can do this with `gcloud auth application-default login`)
+
+# Check out the terra-tools repo to a temporary directory so we can use its utilities
+tmp_terra_tools=$(mktemp -d -t terratools-XXXXXXXX)
+git clone -q git@github.com:broadinstitute/terra-tools.git "$tmp_terra_tools"
 
 # Grab the Redcap tokens from Vault
 ENV=dev
 automation=$(vault read -field=token secret/dsde/monster/${ENV}/dog-aging/redcap-tokens/automation)
 env_automation=$(vault read -field=token secret/dsde/monster/${ENV}/dog-aging/redcap-tokens/env_automation)
-​
-# RUN THE EXTRACTION ON GCP, SAVE TO GS PATH
-# HLES
+# EXTRACTION
+#   HLES
 sbt "dog-aging-hles-extraction/runMain org.broadinstitute.monster.dap.HLESurveyExtractionPipeline --apiToken=$automation --pullDataDictionaries=false --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/raw --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=4 --maxNumWorkers=8 --experiments=shuffle_mode=service"
-# CSLB
+#   CSLB
 sbt "dog-aging-hles-extraction/runMain org.broadinstitute.monster.dap.CslbExtractionPipeline --apiToken=$automation --pullDataDictionaries=false --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=4 --maxNumWorkers=8 --experiments=shuffle_mode=service"
-# ENVIRONMENT
+#   ENVIRONMENT
 sbt "dog-aging-hles-extraction/runMain org.broadinstitute.monster.dap.EnvironmentExtractionPipeline --apiToken=$env_automation --pullDataDictionaries=false --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=4 --maxNumWorkers=8 --experiments=shuffle_mode=service"
-​
+
 # TRANSFORMATION
-# HLES
+#   HLES
 sbt "dog-aging-hles-transformation/run --inputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/raw/hles --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/transform --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=6 --maxNumWorkers=10 --experiments=shuffle_mode=service"
-# CSLB
+#   CSLB
 sbt "dog-aging-hles-transformation/run --inputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/raw/cslb --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/transform --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=6 --maxNumWorkers=10 --experiments=shuffle_mode=service"
-# ENVIRONMENT
+#   ENVIRONMENT
 sbt "dog-aging-hles-transformation/run --inputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/raw/environment --outputPrefix=gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/transform --runner=dataflow --project=broad-dsp-monster-dap-dev --region=us-central1 --workerMachineType=n1-standard-1 --autoscalingAlgorithm=THROUGHPUT_BASED --numWorkers=6 --maxNumWorkers=10 --experiments=shuffle_mode=service"
-# ​
-# ​
-# ​
-# # DOWNLOAD TRANSFORM OUTPUT TO LOCAL MACHINE
-# gsutil ls gs://broad-dsp-monster-dap-dev-storage/weekly_refresh/transform/cslb
-# gsutil cp -r gs://broad-dsp-monster-dap-dev-storage/weekly_refresh/transform/cslb /Users/qhoque/DATA/DAP/InitialDataRelease/transform
-# gsutil cp -r gs://broad-dsp-monster-dap-dev-storage/weekly_refresh/transform/environment /Users/qhoque/DATA/DAP/InitialDataRelease/transform
-# ​
-# ​
-# ## RUN CONVERT OUTPUT SCRIPT
-# python ~/GIT/dog-aging-ingest/hack/convert-output-to-tsv.py transform_output tsv_output -d
-# ​
-# ​
-# # upload to Terra
-# cd /Users/qhoque/GIT/terra-tools/scripts/import_large_tsv
-# python /Users/qhoque/GIT/terra-tools/scripts/import_large_tsv/import_large_tsv_python3.py --tsv "/Users/qhoque/DATA/DAP/env/121420/tsv/environment.tsv" --project "workshop-temp" --workspace "Dog Aging Project - Terra Training Workshop"
+
+# convert transformed results to TSV
+mkdir -p ./tsv_output
+./hack/run_in_virtualenv.sh gsutil_reader "./hack/convert-output-to-tsv.py gs://broad-dsp-monster-dap-dev-storage/tmptest/weekly_refresh/transform ./tsv_output/ --debug"
+
+# upload TSVs to terra
+for tsv_path in ./tsv_output/*.tsv; do
+    python "$tmp_terra_tools/scripts/import_large_tsv/import_large_tsv_python3.py" --tsv "$tsv_path" --project "workshop-temp" --workspace "Dog Aging Project - Terra Training Workshop"
+done
+
+rm -rf "$tmp_terra_tools"
