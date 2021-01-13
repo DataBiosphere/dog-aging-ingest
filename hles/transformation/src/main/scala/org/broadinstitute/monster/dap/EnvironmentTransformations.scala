3b5e0fddfca0335699337a7a77a90c918682bb49
Quazi <59845076+quazi-broad@users.noreply.github.com>
2021-01-13 15:29:40 -0500

    QH-DSPDC-1424-split-redcap-event (#100)
    * "Splitting out redcap_event_name into 3 address column - schema changes"
    * adding logic to split redap event in env transformations
    * Make addressMonth and addressYear optional
    * Drop baseline events, rename field
    * Do not pull baseline_arm_1 events during environmental data extraction
    * Unit tests
    * Removed the logic to skip "baseline_arm_1" when parsing redcap_event_name since we are excluding that specific event in the extraction step.
    * Rewrote the defunct unit test to check something of value - the parsing and mapping of a secondary event when a single event is passed.
    Co-authored-by: Drew Herbst <aherbst@broadinstitute.org>

--- hles/transformation/src/main/scala/org/broadinstitute/monster/dap/EnvironmentTransformations.scala
+++ hles/transformation/src/main/scala/org/broadinstitute/monster/dap/EnvironmentTransformations.scala
@@ -10,17 +10,35 @@ object EnvironmentTransformations {
     */
   def mapEnvironment(rawRecord: RawRecord): Option[Environment] = {
     val dogId = rawRecord.id
+    val redcapEventName = rawRecord.getRequired("redcap_event_name").split("_")
+
+    // set address sequence
+    val addSeq: String =
+      redcapEventName(1) match {
+        case "arm"       => "1"
+        case "secondary" => "2"
+      }
+
+    // set address month
+    val addMonth = redcapEventName(0).filterNot(_.isDigit)
+
+    // set address year
+    val addYear = redcapEventName(0).filter(_.isDigit)
+
     Some(
       Environment(
         dogId = dogId,
-        addressMonthYear = rawRecord.getRequired("redcap_event_name"),
+        addressMonth = addMonth,
+        addressYear = addYear,
         environmentGeocoding = Some(GeocodingTransformations.mapGeocodingMetadata(rawRecord)),
         environmentCensus = Some(CensusTransformations.mapCensusVariables(rawRecord)),
         environmentPollutants = Some(PollutantTransformations.mapPollutantVariables(rawRecord)),
         environmentTemperaturePrecipitation = Some(
           TemperaturePrecipitationTransformations.mapTemperaturePrecipitationVariables(rawRecord)
         ),
-        environmentWalkability = Some(WalkabilityTransformations.mapWalkabilityVariables(rawRecord))
+        environmentWalkability =
+          Some(WalkabilityTransformations.mapWalkabilityVariables(rawRecord)),
+        address1Or2 = addSeq
       )
     )
   }
