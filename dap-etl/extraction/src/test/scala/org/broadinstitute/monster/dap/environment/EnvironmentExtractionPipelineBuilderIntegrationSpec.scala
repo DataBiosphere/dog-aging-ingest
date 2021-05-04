4593e87c684ff97cbf712202bc19be55ee426e34
Quazi Hoque <qhoque@broadinstitute.org>
2021-05-04 13:42:04 -0400

    Added an integration test for Environment that does not appear to be working.

---
+++ dap-etl/extraction/src/test/scala/org/broadinstitute/monster/dap/environment/EnvironmentExtractionPipelineBuilderIntegrationSpec.scala
@@ -0,0 +1,73 @@
+package org.broadinstitute.monster.dap.environment
+
+import better.files.File
+import org.broadinstitute.monster.common.PipelineBuilderSpec
+import org.broadinstitute.monster.dap.common._
+import org.broadinstitute.monster.dap.common
+
+import java.time.{LocalDate, LocalTime, OffsetDateTime, ZoneOffset}
+
+class EnvironmentExtractionPipelineBuilderIntegrationSpec extends PipelineBuilderSpec[Args] {
+  val outputDir = File.newTemporaryDirectory()
+  val envOutputDir = File(outputDir, EnvironmentExtractionPipeline.subdir)
+  override def afterAll(): Unit = outputDir.delete()
+
+  val start =
+    OffsetDateTime.of(LocalDate.of(2020, 11, 15), LocalTime.MIDNIGHT, ZoneOffset.UTC)
+
+  val end =
+    OffsetDateTime.of(LocalDate.of(2020, 11, 16), LocalTime.MIDNIGHT, ZoneOffset.UTC)
+
+  override val testArgs =
+    Args(
+      "token-token-bo-boken",
+      Some(start),
+      Some(end),
+      outputDir.pathAsString,
+      pullDataDictionaries = false
+    )
+
+  val expectedStudyIds = List("some_study_id", "some_other_id")
+
+  val studyIdsRequest = RedcapRequestGeneratorParams(
+    testArgs.apiToken,
+    EnvironmentExtractionPipeline.extractionArmsGenerator(testArgs.startTime, testArgs.endTime),
+    GetRecords(
+      fields = List("study_id"),
+      filters = EnvironmentExtractionPipeline.extractionFiltersGenerator(testArgs)
+    )
+  )
+
+  val followUpRecords = common.RedcapRequestGeneratorParams(
+    testArgs.apiToken,
+    EnvironmentExtractionPipeline.extractionArmsGenerator(testArgs.startTime, testArgs.endTime),
+    GetRecords(
+      ids = expectedStudyIds,
+      fields = EnvironmentExtractionPipeline.fieldList,
+      forms = EnvironmentExtractionPipeline.forms
+    )
+  )
+
+  val mockWrapper = new MockOkWrapper(
+    Map(
+      studyIdsRequest -> RedcapMsgGenerator.toRedcapFormat(expectedStudyIds.map { i =>
+        Map(
+          "study_id" -> i.toString
+        )
+      }),
+      followUpRecords -> RedcapMsgGenerator.toRedcapFormat(expectedStudyIds.map { _ =>
+        Map(
+          "baseline_complete" -> "2"
+        )
+      })
+    )
+  )
+
+  override val builder = EnvironmentExtractionPipeline.buildPipelineWithWrapper(mockWrapper)
+
+  behavior of "EnvironmentSurveyExtractionPipelineBuilder"
+
+  it should "successfully download records from RedCap" in {
+    readMsgs(envOutputDir, "records/*.json") shouldNot be(empty)
+  }
+}
