7865a9f24f702bc22e1552c0b5c24db3c649e463
Eric Park <77357127+eparkmonster@users.noreply.github.com>
2021-06-07 12:11:41 -0400

    Update Sample Transformations

--- dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformations.scala
+++ dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformations.scala
@@ -6,15 +6,20 @@ import org.broadinstitute.monster.dap.common.RawRecord
 object SampleTransformations {
 
   def mapSampleData(rawRecord: RawRecord): Option[Sample] = {
-    case Some(dogId) =>
-      Sample(
-        dogId = rawRecord.getRequired("study_id").toLong,
-        cohort = rawRecord.getOptionalNumber("ce_enroll_stat"),
-        sampleId = rawRecord.getRequired("k1_tube_serial"),
-        dateCollected = rawRecord.getOptionalDate("k1_rtn_tracking_date")
-      )
-    case _ =>
-      None
+    rawRecord.getOptionalDateTime("k1_rtn_tracking_date") match {
+      case Some(dateCollected) =>
+        Some(
+          Sample(
+            dogId = rawRecord.getRequired("study_id").toLong,
+            cohort = rawRecord.getRequired("ce_enroll_stat").toLong,
+            sampleId = Some(rawRecord.getRequired("k1_tube_serial").toLong),
+            sampleType = "saliva_DNA_lowcov",
+            dateCollected = dateCollected
+          )
+        )
+      case _ =>
+        None
+    }
   }
 
 }
