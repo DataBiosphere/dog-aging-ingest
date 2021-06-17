279c0be84104328f4233535a7f11dc7f0669d597
Eric Park <77357127+eparkmonster@users.noreply.github.com>
2021-06-17 15:14:00 -0400

    Create EolsExtractionPipeline

---
+++ dap-etl/extraction/src/main/scala/org/broadinstitute/monster/dap/eols/EolsExtractionPipeline.scala
@@ -0,0 +1,58 @@
+package org.broadinstitute.monster.dap.eols
+
+import org.broadinstitute.monster.common.{PipelineBuilder, ScioApp}
+import org.broadinstitute.monster.dap.common.{
+  Args,
+  ExtractionPipelineBuilder,
+  FilterDirective,
+  FilterOps,
+  HttpWrapper,
+  OkWrapper,
+  RedCapClient
+}
+
+// Ignore IntelliJ, this is used to make the implicit parser compile.
+import Args._
+import java.time.{OffsetDateTime, ZoneOffset}
+
+object EolsExtractionPipeline extends ScioApp[Args] {
+  // january 1, 2018 - we ignore any records before this by default (though there shouldn't be any)
+  val EOLSEpoch = OffsetDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneOffset.ofHours(-5))
+
+  val forms = List(
+    "end_of_life"
+  )
+
+  def extractionFiltersGenerator(args: Args): List[FilterDirective] =
+    args.startTime
+      .map(start =>
+        List(
+          FilterDirective("eol_date_dog_died", FilterOps.>, RedCapClient.redcapFormatDate(start))
+        )
+      )
+      .getOrElse(List()) ++
+      args.endTime
+        .map(end =>
+          List(
+            FilterDirective("eol_date_dog_died", FilterOps.<, RedCapClient.redcapFormatDate(end))
+          )
+        )
+        .getOrElse(List())
+
+  val subdir = "eols";
+  val arm = "baseline_arm_1"
+  val fieldList = List("eol_willing_to_complete", "eol_date_dog_died")
+
+  def buildPipelineWithWrapper(wrapper: HttpWrapper): PipelineBuilder[Args] =
+    new ExtractionPipelineBuilder(
+      forms,
+      extractionFiltersGenerator,
+      (_, _) => List(arm),
+      fieldList,
+      subdir,
+      100,
+      RedCapClient.apply(_: List[String], wrapper)
+    )
+
+  override def pipelineBuilder: PipelineBuilder[Args] = buildPipelineWithWrapper(new OkWrapper())
+}
