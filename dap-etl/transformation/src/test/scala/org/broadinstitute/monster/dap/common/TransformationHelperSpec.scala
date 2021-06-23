d254b286b935387ef91efab7e0718ddd105fe2c6
Drew Herbst <aherbst@broadinstitute.org>
2021-06-23 16:05:25 -0400

    Add test

--- dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/common/TransformationHelperSpec.scala
+++ dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/common/TransformationHelperSpec.scala
@@ -1,13 +1,39 @@
 package org.broadinstitute.monster.dap.common
 
-import org.scalatest.flatspec.AnyFlatSpec
+import com.spotify.scio.testing.PipelineSpec
+import org.apache.beam.sdk.options.{PipelineOptions, PipelineOptionsFactory}
 import org.scalatest.matchers.should.Matchers
 
-class TransformationHelperSpec extends AnyFlatSpec with Matchers {
+class TransformationHelperSpec extends PipelineSpec with Matchers {
   behavior of "TransformationHelper"
 
   it should "map JSON to a raw record grouped by dog ID" in {
+    val path = this.getClass.getResource("transformation_helper").toString
+    val opts: PipelineOptions = PipelineOptionsFactory.fromArgs("--runner=DirectRunner").create()
 
-    false shouldBe true
+    runWithRealContext(opts) { sc =>
+      val records = TransformationHelper.readRecords(
+        sc,
+        path
+      )
+      records should haveSize(3)
+      val ids = records.map(record => record.id)
+      ids should containInAnyOrder(Seq(11111L, 22222L, 33333L))
+    }
+  }
+
+  it should "map JSON to a raw record grouped by dog ID and event name" in {
+    val path = this.getClass.getResource("transformation_helper").toString
+    val opts: PipelineOptions = PipelineOptionsFactory.fromArgs("--runner=DirectRunner").create()
+
+    runWithRealContext(opts) { sc =>
+      val records = TransformationHelper.readRecordsGroupByEventName(
+        sc,
+        path
+      )
+      records should haveSize(6)
+      val ids = records.map(record => record.id)
+      ids should containInAnyOrder(Seq(11111L, 11111L, 22222L, 22222L, 33333L, 33333L))
+    }
   }
 }
