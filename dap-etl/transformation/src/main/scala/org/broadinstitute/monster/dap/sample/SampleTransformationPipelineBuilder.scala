ef8a77841a79bbb07063c7898d03c51a7a9d0bf2
Drew Herbst <aherbst@broadinstitute.org>
2021-06-23 16:09:47 -0400

    Switch to using TransformationHelper methods

--- dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformationPipelineBuilder.scala
+++ dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformationPipelineBuilder.scala
@@ -2,9 +2,8 @@ package org.broadinstitute.monster.dap.sample
 
 import com.spotify.scio.ScioContext
 import com.spotify.scio.values.SCollection
-import org.broadinstitute.monster.common.msg.MsgOps
 import org.broadinstitute.monster.common.{PipelineBuilder, StorageIO}
-import org.broadinstitute.monster.dap.common.{Args, RawRecord}
+import org.broadinstitute.monster.dap.common.{Args, RawRecord, TransformationHelper}
 import org.slf4j.{Logger, LoggerFactory}
 
 object SampleTransformationPipelineBuilder extends PipelineBuilder[Args] {
@@ -26,27 +25,7 @@ object SampleTransformationPipelineBuilder extends PipelineBuilder[Args] {
 
   /** Read in records and group by study Id, with field name subgroups. */
   def readRecords(ctx: ScioContext, args: Args): SCollection[RawRecord] = {
-    val rawRecords = StorageIO
-      .readJsonLists(
-        ctx,
-        "Raw Records",
-        s"${args.inputPrefix}/records/*.json"
-      )
-
-    // Group by study ID (record number) and field name
-    // to get the format: (studyId, Iterable((fieldName, Iterable(value))))
-    rawRecords
-      .groupBy(_.read[String]("record"))
-      .map {
-        case (id, rawRecordValues) =>
-          val fields = rawRecordValues
-            .groupBy(_.read[String]("field_name"))
-            .map {
-              case (fieldName, rawValues) =>
-                (fieldName, rawValues.map(_.read[String]("value")).toArray.sorted)
-            }
-          RawRecord(id.toLong, fields)
-      }
+    TransformationHelper.readRecords(ctx, args.inputPrefix)
   }
 
 }
