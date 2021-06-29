33d61dc4b4598c55a4397e636bcb16b79cfbdb19
Eric Park <77357127+eparkmonster@users.noreply.github.com>
2021-06-29 14:41:38 -0400

    Create Death transformations

---
+++ dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/eols/DeathTransformations.scala
@@ -0,0 +1,30 @@
+package org.broadinstitute.monster.dap.eols
+
+import org.broadinstitute.monster.dap.common.RawRecord
+import org.broadinstitute.monster.dogaging.jadeschema.fragment.EolsDeath
+
+object DeathTransformations {
+
+  def mapDeathTransformations(rawRecord: RawRecord): EolsDeath = {
+    val deathLocation = rawRecord.getOptionalNumber("eol_death_location")
+    val deathWitness = rawRecord.fields.get("eol_who_present")
+    EolsDeath(
+      eolDeathLocation = deathLocation,
+      eolDeathLocationOtherDescription =
+        if (deathLocation.contains(98))
+          rawRecord.getOptional("eol_death_location_specify")
+        else None,
+      eolDeathWitness = rawRecord.getOptionalNumber("eol_anyone_present"),
+      eolDeathWitnessWhoYou = deathWitness.map(_.contains("1")),
+      eolDeathWitnessWhoFamily = deathWitness.map(_.contains("2")),
+      eolDeathWitnessWhoAcquaintance = deathWitness.map(_.contains("3")),
+      eolDeathWitnessWhoVet = deathWitness.map(_.contains("4")),
+      eolDeathWitnessWhoBoarder = deathWitness.map(_.contains("5")),
+      eolDeathWitnessWhoOther = deathWitness.map(_.contains("98")),
+      eolDeathWitnessWhoOtherDescription =
+        if (deathWitness.map(_.contains("98")).contains(true))
+          rawRecord.getOptional("eol_who_present_specify")
+        else None
+    )
+  }
+}
