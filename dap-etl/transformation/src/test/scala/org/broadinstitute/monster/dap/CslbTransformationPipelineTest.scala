38411ddead44497e07ba16f940b61fb17e3453c3
Quazi Hoque <qhoque@broadinstitute.org>
2021-07-22 15:44:08 -0400

    Added a pipeline test for the CSLB transformation pipeline including tests for valid/invalid cslb records. Added three files of fixture data to test the CSLB edge cases.

--- dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/CslbTransformationPipelineTest.scala
+++ dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/CslbTransformationPipelineTest.scala
@@ -9,13 +9,23 @@ import scala.io.Source
 
 class CslbTransformationPipelineTest extends PipelineSpec with Matchers {
 
-  it should "do something" in {
-    val lines = Source.fromResource("cslb.json").getLines().toSeq
-    val expected = Seq("a: 1")
+  it should "process a valid cslb record" in {
+    val lines =
+      Source.fromResource("cslb_valid.json").getLines().toSeq
+    val expected = Source.fromResource("cslb_valid_output.json").getLines().toSeq
     JobTest[CslbTransformationPipeline.type]
       .args("--inputPrefix=in", "--outputPrefix=out")
       .input(TextIO("in/records/*.json"), lines)
       .output(TextIO("out/cslb"))(col => col should containInAnyOrder(expected))
+      .run()
+  }
+
+  it should "not process a cslb record without cslb_date" in {
+    val lines = Source.fromResource("cslb_missing_date.json").getLines().toSeq
+    JobTest[CslbTransformationPipeline.type]
+      .args("--inputPrefix=in", "--outputPrefix=out")
+      .input(TextIO("in/records/*.json"), lines)
+      .output(TextIO("out/cslb"))(col => col should beEmpty)
       .run()
   }
 }
