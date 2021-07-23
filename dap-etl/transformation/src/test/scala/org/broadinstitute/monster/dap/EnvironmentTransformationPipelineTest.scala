9e1f4bdc4c2f67f32278eb038f2bede84d750ecc
Quazi Hoque <qhoque@broadinstitute.org>
2021-07-23 16:42:26 -0400

    Added a pipeline test for the Environment transformation pipeline including tests for valid/invalid environment records. Renamed some of the fixture data files for clarity.

---
+++ dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/EnvironmentTransformationPipelineTest.scala
@@ -0,0 +1,39 @@
+package org.broadinstitute.monster.dap
+
+import com.spotify.scio.io.TextIO
+import com.spotify.scio.testing.PipelineSpec
+import org.apache.beam.sdk.Pipeline.PipelineExecutionException
+import org.broadinstitute.monster.dap.environment.EnvironmentTransformationPipeline
+import org.scalatest.matchers.should.Matchers
+
+import scala.io.Source
+
+class EnvironmentTransformationPipelineTest extends PipelineSpec with Matchers {
+
+  it should "process a valid environment record and process data" in {
+    val lines =
+      Source.fromResource("environment_valid.json").getLines().toSeq
+    val expected_environment = Source.fromResource("environment_valid_output.json").getLines().toSeq
+
+    JobTest[EnvironmentTransformationPipeline.type]
+      .args("--inputPrefix=in", "--outputPrefix=out")
+      .input(TextIO("in/records/*.json"), lines)
+      .output(TextIO("out/environment"))(col => col should containInAnyOrder(expected_environment))
+      .run()
+  }
+
+  // todo: missing redcap_event_name
+  it should "not process a environment record with an invalid redcap_event_name" in {
+    val lines =
+      Source.fromResource("environment_missing_event.json").getLines().toSeq
+    val expected_environment = Source.fromResource("environment_valid_output.json").getLines().toSeq
+
+    assertThrows[PipelineExecutionException] {
+      JobTest[EnvironmentTransformationPipeline.type]
+        .args("--inputPrefix=in", "--outputPrefix=out")
+        .input(TextIO("in/records/*.json"), lines)
+        .output(TextIO("out/environment"))(col => col should containInAnyOrder(expected_environment))
+        .run()
+    }
+  }
+}
--- dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/HlesTransformationPipelineTest.scala
+++ dap-etl/transformation/src/test/scala/org/broadinstitute/monster/dap/HlesTransformationPipelineTest.scala
@@ -2,6 +2,7 @@ package org.broadinstitute.monster.dap
 
 import com.spotify.scio.io.TextIO
 import com.spotify.scio.testing.PipelineSpec
+import org.broadinstitute.monster.dap.common.HLESurveyTransformationFailException
 import org.broadinstitute.monster.dap.hles.HLESurveyTransformationPipeline
 import org.scalatest.matchers.should.Matchers
 
@@ -9,15 +10,18 @@ import scala.io.Source
 
 class HlesTransformationPipelineTest extends PipelineSpec with Matchers {
 
-  it should "process a valid hles record and process dog data" in {
+  val expected_hles_dog = Source.fromResource("hles_valid_dog.json").getLines().toSeq
+  val expected_hles_owner = Source.fromResource("hles_valid_owner.json").getLines().toSeq
+
+  val expected_hles_health_condition =
+    Source.fromResource("hles_valid_health_condition.json").getLines().toSeq
+
+  val expected_hles_cancer_condition =
+    Source.fromResource("hles_valid_cancer_condition.json").getLines().toSeq
+
+  it should "process a valid hles record and process all dog data" in {
     val lines =
       Source.fromResource("hles_valid.json").getLines().toSeq
-    val expected_hles_dog = Source.fromResource("hles_dog_valid_output.json").getLines().toSeq
-    val expected_hles_owner = Source.fromResource("hles_owner_valid_output.json").getLines().toSeq
-    val expected_hles_health_condition =
-      Source.fromResource("hles_health_condition_valid_output.json").getLines().toSeq
-    val expected_hles_cancer_condition =
-      Source.fromResource("hles_cancer_condition_valid_output.json").getLines().toSeq
 
     JobTest[HLESurveyTransformationPipeline.type]
       .args("--inputPrefix=in", "--outputPrefix=out")
@@ -36,28 +40,40 @@ class HlesTransformationPipelineTest extends PipelineSpec with Matchers {
   it should "not process an hles_dog or hles_owner record without st_owner_id" in {
     val lines =
       Source.fromResource("hles_missing_owner.json").getLines().toSeq
-    val expected_hles_health_condition =
-      Source.fromResource("hles_health_condition_valid_output.json").getLines().toSeq
-    val expected_hles_cancer_condition =
-      Source.fromResource("hles_cancer_condition_valid_output.json").getLines().toSeq
+
+    assertThrows[HLESurveyTransformationFailException] {
+      JobTest[HLESurveyTransformationPipeline.type]
+        .args("--inputPrefix=in", "--outputPrefix=out")
+        .input(TextIO("in/records/*.json"), lines)
+        .output(TextIO("out/hles_dog"))(col => col should beEmpty)
+        .output(TextIO("out/hles_owner"))(col => col should beEmpty)
+        .output(TextIO("out/hles_health_condition"))(col =>
+          col should containInAnyOrder(expected_hles_health_condition)
+        )
+        .output(TextIO("out/hles_cancer_condition"))(col =>
+          col should containInAnyOrder(expected_hles_cancer_condition)
+        )
+        .run()
+    }
+  }
+
+  it should "not process cancer conditions if not specified" in {
+    val lines =
+      Source.fromResource("hles_no_cancer.json").getLines().toSeq
+    val expected_hles_dog_no_cancer =
+      Source.fromResource("hles_no_cancer_dog.json").getLines().toSeq
 
     JobTest[HLESurveyTransformationPipeline.type]
       .args("--inputPrefix=in", "--outputPrefix=out")
       .input(TextIO("in/records/*.json"), lines)
-      .output(TextIO("out/hles_dog"))(col => col should beEmpty)
-      .output(TextIO("out/hles_owner"))(col => col should beEmpty)
-      // I don't think we are writing records missing owner_id to the output
-      // but I see an error when I mark the following two outputs as "should beEmpty"
-      // is there a way to catch an exception here?
+      .output(TextIO("out/hles_dog"))(col =>
+        col should containInAnyOrder(expected_hles_dog_no_cancer)
+      )
+      .output(TextIO("out/hles_owner"))(col => col should containInAnyOrder(expected_hles_owner))
       .output(TextIO("out/hles_health_condition"))(col =>
         col should containInAnyOrder(expected_hles_health_condition)
       )
-      .output(TextIO("out/hles_cancer_condition"))(col =>
-        col should containInAnyOrder(expected_hles_cancer_condition)
-      )
+      .output(TextIO("out/hles_cancer_condition"))(col => col should beEmpty)
       .run()
   }
-
-  // todo: add a test for a record without cancer data: if (rawRecord.getBoolean("hs_dx_cancer_yn"))
-  // todo: add a test for a record without a congenital health condition: if (rawRecord.getBoolean("hs_congenital_yn"))
 }
