585736a874db75a53d66913640450c37faac82d4
Quazi Hoque <qhoque@broadinstitute.org>
2021-08-16 17:03:46 -0400

    Reverting a logging change that was made for testing. Added a custom DateTimeFormatterBuilder instead of mapping the nonstandard months for a cleaner implementation.

--- dap-etl/extraction/src/main/scala/org/broadinstitute/monster/dap/environment/EnvironmentExtractionPipeline.scala
+++ dap-etl/extraction/src/main/scala/org/broadinstitute/monster/dap/environment/EnvironmentExtractionPipeline.scala
@@ -4,7 +4,9 @@ import org.broadinstitute.monster.common.{PipelineBuilder, ScioApp}
 import org.broadinstitute.monster.dap.common._
 
 import java.time.{OffsetDateTime, ZoneOffset}
-import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoField
+import java.time.format.DateTimeFormatterBuilder
+import collection.JavaConverters._
 
 // Ignore IntelliJ, this is used to make the implicit parser compile.
 import Args._
@@ -13,9 +15,9 @@ class EnvironmentExtractionFailException() extends Exception
 
 object EnvironmentExtractionPipeline extends ScioApp[Args] {
 
-  val formatter = DateTimeFormatter.ofPattern("MMMyyyy")
-  val formatter_month = DateTimeFormatter.ofPattern("MMMM")
-  val formatter_year = DateTimeFormatter.ofPattern("yyyy")
+  //val formatter = DateTimeFormatter.ofPattern("MMMyyyy")
+  //val formatter_month = DateTimeFormatter.ofPattern("MMMM")
+  //val formatter_year = DateTimeFormatter.ofPattern("yyyy")
 
   val EnvironmentEpoch = OffsetDateTime.of(2020, 1, 1, 0, 0, 0, 0, ZoneOffset.ofHours(-5))
 
@@ -44,20 +46,34 @@ object EnvironmentExtractionPipeline extends ScioApp[Args] {
 
   val subdir = "environment"
 
+  // Utilizing the custom date formatter since June, July, and Sept have non standard abbreviations
+  val monthMap: java.util.Map[java.lang.Long, String] = Map(
+    Long.box(1L) -> "Jan",
+    Long.box(2L) -> "Feb",
+    Long.box(3L) -> "Mar",
+    Long.box(4L) -> "Apr",
+    Long.box(5L) -> "May",
+    Long.box(6L) -> "June",
+    Long.box(7L) -> "July",
+    Long.box(8L) -> "Aug",
+    Long.box(9L) -> "Sept",
+    Long.box(10L) -> "Oct",
+    Long.box(11L) -> "Nov",
+    Long.box(12L) -> "Dec"
+  ).asJava
+
+  val formatter = new DateTimeFormatterBuilder()
+    .appendText(ChronoField.MONTH_OF_YEAR, monthMap)
+    .appendPattern("yyyy")
+    .toFormatter()
+
   // get list of individual dates, then get the set of years and return a list of distinct monthYears
   def getMonthYearList(start: OffsetDateTime, end: OffsetDateTime): List[String] = {
     if (start.isAfter(end)) throw new EnvironmentExtractionFailException
     val dateList =
       (Iterator.iterate(start)(_ plusDays 1) takeWhile (_ isBefore end.plusDays(1))).toList
     dateList
-      .map(date =>
-        // event names for June, July, and Sept have 4 alpha characters before the year
-        if (List(6, 7, 9).contains(date.getMonthValue)) {
-          date.format(formatter_month).toLowerCase.slice(0, 4) + date
-            .format(formatter_year)
-            .toLowerCase
-        } else date.format(formatter).toLowerCase
-      )
+      .map(date => date.format(formatter).toLowerCase)
       .distinct
   }
 
