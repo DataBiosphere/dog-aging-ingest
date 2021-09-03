762e84baa91cdfd0fb7f118d0f7d6b0bb5c7bffc
Drew Herbst <aherbst@broadinstitute.org>
2021-09-03 17:31:08 -0400

    Fix

--- orchestration/dap_orchestration/solids.py
+++ orchestration/dap_orchestration/solids.py
@@ -105,7 +105,7 @@ def env_extract_records(context: AbstractComputeExecutionContext) -> DapSurveyTy
     context.resources.extract_beam_runner.run(arg_dict,
                                               target_class=f"{class_prefix}.environment.EnvironmentExtractionPipeline",
                                               scala_project=extract_project,
-                                              command=[f"/app/bin/environment-transformation-pipeline"]
+                                              command=[f"/app/bin/environment-extraction-pipeline"]
                                               )
     return DapSurveyType("environment")
 
