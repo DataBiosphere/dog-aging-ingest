b800e76bf8e8f42adaf4229a0aab860c3c9f2c05
Drew Herbst <aherbst@broadinstitute.org>
2021-10-19 14:14:34 -0400

    Add missing import

--- orchestration/dap_orchestration/solids.py
+++ orchestration/dap_orchestration/solids.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from google.cloud.storage.client import Client
 from google.cloud.storage.blob import Blob
-from dagster import Bool, Failure, String, solid, configured, InputDefinition, Nothing
+from dagster import Bool, Failure, String, solid, configured, InputDefinition, Nothing, Noneable
 from dagster.core.execution.context.compute import AbstractComputeExecutionContext
 from dagster_utils.contrib.google import parse_gs_path
 from google.cloud.storage.blob import Blob
