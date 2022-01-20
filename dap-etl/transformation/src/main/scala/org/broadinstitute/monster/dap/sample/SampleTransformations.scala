a8e858c4fadf5beaaa7ba0b33cdcb3093e25b10f
Quazi <59845076+quazi-broad@users.noreply.github.com>
2022-01-20 11:42:15 -0500

    Add filter on the samples pipeline for k1_tube_serial (#226)

--- dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformations.scala
+++ dap-etl/transformation/src/main/scala/org/broadinstitute/monster/dap/sample/SampleTransformations.scala
@@ -1,18 +1,20 @@
 package org.broadinstitute.monster.dap.sample
 
-import org.broadinstitute.monster.dogaging.jadeschema.table.Sample
 import org.broadinstitute.monster.dap.common.RawRecord
+import org.broadinstitute.monster.dogaging.jadeschema.table.Sample
 
 object SampleTransformations {
 
   def mapSampleData(rawRecord: RawRecord): Option[Sample] = {
-    rawRecord.getOptionalDateTime("k1_rtn_tracking_date") match {
-      case Some(dateCollected) =>
+    val dateCollected = rawRecord.getOptionalDateTime("k1_rtn_tracking_date")
+    val kitId = rawRecord.getOptionalNumber("k1_tube_serial")
+    (dateCollected, kitId) match {
+      case (Some(dateCollected), Some(kitId)) =>
         Some(
           Sample(
             dogId = rawRecord.getRequired("study_id").toLong,
             cohort = rawRecord.getRequired("ce_enroll_stat").toLong,
-            sampleId = rawRecord.getRequired("k1_tube_serial").toLong,
+            sampleId = kitId,
             sampleType = "saliva_DNA_lowcov",
             dateSwabArrivalLaboratory = dateCollected
           )
